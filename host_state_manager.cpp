624ddba1be1ea5f143db8de315e0e04622e52a78
Andrew Geissler <andrewg@us.ibm.com>
2016-12-01 12:33:09 -0600

    Determine host state on startup and set internally
    This code brings some coherence to the internal host state
    and transition values within the host state manager
    
    Change-Id: I989a40bb01d5c1c16c7e42cceac1bc99ce3222f4
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

--- host_state_manager.cpp
+++ host_state_manager.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <systemd/sd-bus.h>
 #include "host_state_manager.hpp"
 
 namespace phosphor
@@ -8,6 +9,8 @@ namespace state
 namespace manager
 {
 
+using namespace sdbusplus::xyz::openbmc_project::State::server;
+
 Host::Host(
         sdbusplus::bus::bus& bus,
         const char* busName,
@@ -16,7 +19,91 @@ Host::Host(
             sdbusplus::xyz::openbmc_project::State::server::Host>(
                bus, objPath),
          bus(bus),
-         path(objPath) {}
+         path(objPath)
+{
+    determineInitialState();
+}
+
+// TODO - Will be rewritten once sdbusplus client bindings are in place
+void Host::determineInitialState()
+{
+    int rc = 0;
+    int pgood = -1;
+    sd_bus_message *response = NULL;
+    sd_bus_error busError = SD_BUS_ERROR_NULL;
+    sd_bus *bus = NULL;
+    rc = sd_bus_open_system(&bus);
+    if(rc < 0)
+    {
+        std::cerr << "Failed to open system bus" << std::endl;
+        goto finish;
+    }
+
+    rc = sd_bus_get_property(bus,
+                             "org.openbmc.control.Power",
+                             "/org/openbmc/control/power0",
+                             "org.openbmc.control.Power",
+                             "pgood",
+                             &busError,
+                             &response,
+                             "i");
+    if(rc < 0)
+    {
+        std::cerr << "Failed to read pgood property" << std::endl;
+        goto finish;
+    }
+
+    rc = sd_bus_message_read(response, "i", &pgood);
+    if(rc < 0)
+    {
+        std::cerr << "Failed to extract pgood from msg" << std::endl;
+        goto finish;
+    }
+
+    if(pgood==1)
+    {
+        std::cout << "HOST is BOOTED " << pgood << std::endl;
+        sdbusplus::xyz::openbmc_project::State::server::Host::
+            currentHostState(HostState::Running);
+    }
+    else
+    {
+        std::cout << "HOST is not BOOTED " << pgood << std::endl;
+        sdbusplus::xyz::openbmc_project::State::server::Host::
+            currentHostState(HostState::Off);
+    }
+
+    // Set transition initially to Off
+    // TODO - Eventually need to restore this from persistent storage
+    sdbusplus::xyz::openbmc_project::State::server::Host::
+                requestedHostTransition(Transition::Off);
+
+finish:
+    sd_bus_error_free(&busError);
+    response = sd_bus_message_unref(response);
+    sd_bus_unref(bus);
+
+    return;
+}
+
+Host::Transition Host::requestedHostTransition(Transition value)
+{
+    std::cout << "Someone is setting the RequestedHostTransition field" <<
+            std::endl;
+    return sdbusplus::xyz::openbmc_project::State::server::Host::
+            requestedHostTransition(value);
+}
+
+
+Host::HostState Host::currentHostState(HostState value)
+{
+    std::cout << "Someone is being bad and trying to set the HostState field" <<
+            std::endl;
+
+    return sdbusplus::xyz::openbmc_project::State::server::Host::
+            currentHostState();
+}
+>>>>>>> 461858e... Determine host state on startup and set internally
 
 } // namespace manager
 } // namespace state
