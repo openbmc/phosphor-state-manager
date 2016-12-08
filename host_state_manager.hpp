1918527106bc38fb1eca288c8fc057a7c7d269c8
Andrew Geissler <andrewg@us.ibm.com>
2016-12-08 12:56:21 -0600

    Process system state changes
    Use the system state signal to know when to change the state
    of the host
    
    The signal we look at may change with future skeleton
    refactors but this is the best signal for now
    
    Change-Id: Ibba7cc5628994425520c21cdf122d2710f8164aa
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

--- host_state_manager.hpp
+++ host_state_manager.hpp
@@ -40,6 +40,10 @@ class Host : public sdbusplus::server::object::object<
                             bus, objPath),
                 bus(bus),
                 path(objPath),
+                stateSignal(bus,
+                            "type='signal',member='GotoSystemState'",
+                            handleSysStateChange,
+                            this),
                 tranActive(false)
         {
             // Will throw exception on fail
@@ -80,11 +84,28 @@ class Host : public sdbusplus::server::object::object<
          */
         void executeTransition(Transition tranReq);
 
+        /** @brief Callback function on system state changes
+         *
+         *  Check if the state is relevant to the Host and if so, update
+         *  corresponding host object's state
+         *
+         * @param[in] msg        - Data associated with subscribed signal
+         * @param[in] userData   - Pointer to this object instance
+         * @param[in] retError   - Return error data
+         *
+         */
+        static int handleSysStateChange(sd_bus_message* msg,
+                                        void* userData,
+                                        sd_bus_error* retError);
+
         /** @brief Persistent sdbusplus DBus bus connection. */
         sdbusplus::bus::bus& bus;
 
         /** @brief Path of the host instance */
         std::string path;
+
+        /** @brief Used to subscribe to dbus system state changes */
+        sdbusplus::server::match::match stateSignal;
 
         /** @brief Indicates whether transition is actively executing */
         bool tranActive;
