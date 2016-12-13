53ad75c8fc640f9f881875001b833dd7b7e03850
Andrew Geissler <andrewg@us.ibm.com>
2016-12-13 16:16:28 -0600

    Initial chassis state management code
    This is just the basics to get the generated code
    compiling and to verify the basic dbus interfaces.
    
    Change-Id: I335964da456d8173e2ce792c6614770da43303b4
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

---
+++ chassis_state_manager.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <systemd/sd-bus.h>
+#include <log.hpp>
+#include "chassis_state_manager.hpp"
+
+namespace phosphor
+{
+namespace state
+{
+namespace manager
+{
+
+// When you see server:: you know we're referencing our base class
+namespace server = sdbusplus::xyz::openbmc_project::State::server;
+
+using namespace phosphor::logging;
+
+Chassis::Transition Chassis::requestedPowerTransition(Transition value)
+{
+
+    log<level::INFO>("Change to Chassis Requested Power State",
+                     entry("CHASSIS_REQUESTED_POWER_STATE=%s",
+                           convertForMessage(value).c_str()));
+    return server::Chassis::requestedPowerTransition(value);
+}
+
+Chassis::PowerState Chassis::currentPowerState(PowerState value)
+{
+    log<level::INFO>("Change to Chassis Power State",
+                     entry("CURRENT_CHASSIS_POWER_STATE=%s",
+                           convertForMessage(value).c_str()));
+    return server::Chassis::currentPowerState(value);
+}
+
+} // namespace manager
+} // namespace state
+} // namepsace phosphor
