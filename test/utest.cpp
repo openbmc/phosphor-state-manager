0ca9b43fdc904de150599e85676064f5f6ae10a0
Andrew Geissler <andrewg@us.ibm.com>
2016-12-13 11:46:16 -0600

    Support reboot transition request in host interface
    Change-Id: Ic09ccf710f13697fd203ca1b96c3b5084a76511b
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

--- test/utest.cpp
+++ test/utest.cpp
@@ -6,44 +6,87 @@ using namespace sdbusplus::xyz::openbmc_project::State;
 
 /* Begin verifyValidTransition() test cases */
 TEST(VerifyValidTransition, TransitionActive) {
+    server::Host::Transition tranReq;
     bool verify = Host::verifyValidTransition(server::Host::Transition::Off,
                                               Host::HostState::Running,
-                                              true);
+                                              true,
+                                              tranReq);
     EXPECT_EQ(verify, false);
 }
 
 TEST(VerifyValidTransition, AlreadyAtStateOff) {
+    server::Host::Transition tranReq;
     bool verify = Host::verifyValidTransition(server::Host::Transition::Off,
                                               Host::HostState::Off,
-                                              false);
+                                              false,
+                                              tranReq);
     EXPECT_EQ(verify, false);
 }
 
 TEST(VerifyValidTransition, AlreadyAtStateRunning) {
+    server::Host::Transition tranReq;
     bool verify = Host::verifyValidTransition(server::Host::Transition::On,
                                               Host::HostState::Running,
-                                              false);
+                                              false,
+                                              tranReq);
     EXPECT_EQ(verify, false);
 }
 
 TEST(VerifyValidTransition, OffToReboot) {
+    server::Host::Transition tranReq;
     bool verify = Host::verifyValidTransition(server::Host::Transition::Reboot,
                                               Host::HostState::Off,
-                                              false);
+                                              false,
+                                              tranReq);
     EXPECT_EQ(verify, false);
 }
 
 TEST(VerifyValidTransition, RunningToOff) {
+    server::Host::Transition tranReq;
     bool verify = Host::verifyValidTransition(server::Host::Transition::Off,
                                               Host::HostState::Running,
-                                              false);
+                                              false,
+                                              tranReq);
     EXPECT_EQ(verify, true);
+    EXPECT_EQ(tranReq,server::Host::Transition::Off);
 }
 
 TEST(VerifyValidTransition, OffToRunning) {
+    server::Host::Transition tranReq;
     bool verify = Host::verifyValidTransition(server::Host::Transition::On,
                                               Host::HostState::Off,
-                                              false);
+                                              false,
+                                              tranReq);
     EXPECT_EQ(verify, true);
+    EXPECT_EQ(tranReq,server::Host::Transition::On);
+}
+
+TEST(VerifyValidTransition, RunningToRebootTranOn) {
+    server::Host::Transition tranReq;
+    bool verify = Host::verifyValidTransition(server::Host::Transition::Reboot,
+                                              Host::HostState::Running,
+                                              true,
+                                              tranReq);
+    EXPECT_EQ(verify, false);
+}
+
+TEST(VerifyValidTransition, RunningToRebootTranOff) {
+    server::Host::Transition tranReq;
+    bool verify = Host::verifyValidTransition(server::Host::Transition::Reboot,
+                                              Host::HostState::Running,
+                                              false,
+                                              tranReq);
+    EXPECT_EQ(verify, true);
+    EXPECT_EQ(tranReq,server::Host::Transition::Off);
+}
+
+TEST(VerifyValidTransition, OffToRebootTranOn) {
+    server::Host::Transition tranReq;
+    bool verify = Host::verifyValidTransition(server::Host::Transition::Reboot,
+                                              Host::HostState::Off,
+                                              true,
+                                              tranReq);
+    EXPECT_EQ(verify, true);
+    EXPECT_EQ(tranReq,server::Host::Transition::On);
 }
 /* End verifyValidTransition() test cases */
