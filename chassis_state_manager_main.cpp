a90a31a966863ad9abb537c4cccfa07db5533f51
Andrew Geissler <andrewg@us.ibm.com>
2016-12-13 16:16:28 -0600

    Initial chassis state management code
    This is just the basics to get the generated code
    compiling and to verify the basic dbus interfaces.
    
    Change-Id: I335964da456d8173e2ce792c6614770da43303b4
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

--- chassis_state_manager_main.cpp
+++ chassis_state_manager_main.cpp
@@ -3,28 +3,30 @@
 #include <exception>
 #include <sdbusplus/bus.hpp>
 #include "config.h"
-#include "host_state_manager.hpp"
+#include "chassis_state_manager.hpp"
+
 
 int main(int argc, char *argv[])
 {
     auto bus = sdbusplus::bus::new_default();
 
-    // For now, we only have one instance of the host
-    auto objPathInst = std::string{HOST_OBJPATH} + '0';
+    // For now, we only have one instance of the chassis
+    auto objPathInst = std::string{CHASSIS_OBJPATH} + '0';
 
     // Add sdbusplus ObjectManager.
     sdbusplus::server::manager::manager objManager(bus, objPathInst.c_str());
 
-    phosphor::state::manager::Host manager(bus,
-                                           HOST_BUSNAME,
-                                           objPathInst.c_str());
+    phosphor::state::manager::Chassis manager(bus,
+                                              CHASSIS_BUSNAME,
+                                              objPathInst.c_str());
 
-    bus.request_name(HOST_BUSNAME);
+    bus.request_name(CHASSIS_BUSNAME);
 
     while(true)
     {
         bus.process_discard();
         bus.wait();
     }
+
     return 0;
 }
