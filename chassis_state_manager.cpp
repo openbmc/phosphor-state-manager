5203a5e18b280636960d6115250be0dc0e493c56
Andrew Geissler <andrewg@us.ibm.com>
2016-12-13 20:39:04 -0600

    Determine chassis power state on application startup
    I also added the instance support in this commit as well with
    the intention of using it connecti to the correct control/powerX
    but I'm going to wait for official sdbusplus support to be
    in to read properties before using it.
    
    Change-Id: I406b2a6c7af1e4c26af0e43a432a130c967d84bc
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

--- chassis_state_manager.cpp
+++ chassis_state_manager.cpp
@@ -20,16 +20,77 @@ using namespace phosphor::logging;
 
 Chassis::Chassis(
         sdbusplus::bus::bus& bus,
-        const char* busName,
+        int instance,
         const char* objPath) :
         sdbusplus::server::object::object<
             server::Chassis>(
                bus, objPath),
          bus(bus),
-         path(objPath)
-{}
+         instance(instance)
+{
+    determineInitialState();
+}
 
+// TODO - Will be rewritten once sdbusplus client bindings are in place
+//        and persistent storage design is in place and sdbusplus
+//        has read property function
+void Chassis::determineInitialState()
+{
+    int rc = 0;
+    int pgood = -1;
+    sd_bus_message *response = nullptr;
+    sd_bus *bus = nullptr;
+    rc = sd_bus_open_system(&bus);
+    if(rc < 0)
+    {
+        log<level::ERR>("Failed to open system bus");
+        goto finish;
+    }
 
+    rc = sd_bus_get_property(bus,
+                             "org.openbmc.control.Power",
+                             "/org/openbmc/control/power0",
+                             "org.openbmc.control.Power",
+                             "pgood",
+                             nullptr,
+                             &response,
+                             "i");
+    if(rc < 0)
+    {
+        log<level::ERR>("Failed to read pgood property");
+        goto finish;
+    }
+
+    rc = sd_bus_message_read(response, "i", &pgood);
+    if(rc < 0)
+    {
+        log<level::ERR>("Failed to extract pgood from msg");
+        goto finish;
+    }
+
+    if(pgood==1)
+    {
+        log<level::INFO>("Initial Chassis State will be On",
+                         entry("CURRENT_CHASSIS_POWER_STATE=0x%.2X",
+                               PowerState::On));
+        server::Chassis::currentPowerState(PowerState::On);
+        server::Chassis::requestedPowerTransition(Transition::On);
+    }
+    else
+    {
+        log<level::INFO>("Initial Chassis State will be Off",
+                         entry("CURRENT_CHASSIS_POWER_STATE=0x%.2X",
+                               PowerState::Off));
+        server::Chassis::currentPowerState(PowerState::Off);
+        server::Chassis::requestedPowerTransition(Transition::Off);
+    }
+
+finish:
+    response = sd_bus_message_unref(response);
+    sd_bus_unref(bus);
+
+    return;
+}
 
 Chassis::Transition Chassis::requestedPowerTransition(Transition value)
 {
