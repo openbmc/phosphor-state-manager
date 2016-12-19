c3cb97afaeb1d08675ea35c8aabf96244f7d6a81
Josh D. King <jdking@us.ibm.com>
2016-12-19 11:13:43 -0600

    Initial commit for BMC state management
    This is the starting code needing to
    build the application.  Still more to do,
    this is just the layout for the additional
    commits that will be done for the state
    management.
    
    Change-Id: Ief1dc96c84b34630e60ef40c99d4faf5b6f6e02e
    Signed-off-by: Josh D. King <jdking@us.ibm.com>

---
+++ BMC_state_manager.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "BMC_state_manager.hpp"
+
+namespace phosphor
+{
+namespace state
+{
+namespace manager
+{
+
+} // namespace manager
+} // namespace state
+} // namepsace phosphor
+
