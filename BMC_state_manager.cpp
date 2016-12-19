359dd0ccb72dfaccade718b4ecd57848f08499db
Josh D. King <jdking@us.ibm.com>
2016-12-19 14:52:40 -0600

    Determine initial BMC state on startup
    Added code for the implementation of the method
    determineInitialState. Works with the values of
    the internal BMC state and transition values in
    the BMC state manager.
    
    Change-Id: I824b83676db8cb211f5bee954f98a719a773a62b
    Signed-off-by: Josh D. King <jdking@us.ibm.com>

--- BMC_state_manager.cpp
+++ BMC_state_manager.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <systemd/sd-bus.h>
 #include "BMC_state_manager.hpp"
 
 namespace phosphor
@@ -7,6 +8,50 @@ namespace state
 {
 namespace manager
 {
+using namespace sdbusplus::xyz::openbmc_project::State::server;
+
+
+void BMC::determineInitialState()
+{
+    std::string sysState;
+
+    auto method = this->bus.new_method_call("org.openbmc.managers.System",
+                                            "/org/openbmc/managers/System",
+                                            "org.openbmc.managers.System",
+                                            "getSystemState");
+
+    auto reply = this->bus.call(method);
+
+    reply.read(sysState);
+
+    if(sysState == "HOST_BOOTED")
+    {
+        std::cout << "BMC is READY " << sysState << std::endl;
+        currentBMCState(BMCState::Ready);
+    }
+    else
+    {
+        std::cout << "BMC is not READY " << sysState << std::endl;
+        currentBMCState(BMCState::NotReady);
+    }
+
+    return;
+}
+
+BMC::Transition BMC::requestedBMCTransition(Transition value)
+{
+    std::cout << "Setting the RequestedBMCTransition field" << std::endl;
+    return sdbusplus::xyz::openbmc_project::State::server::BMC::
+            requestedBMCTransition(value);
+}
+
+BMC::BMCState BMC::currentBMCState(BMCState value)
+{
+    std::cout << "Setting the BMCState field" << std::endl;
+    return sdbusplus::xyz::openbmc_project::State::server::BMC::
+            currentBMCState(value);
+}
+
 
 } // namespace manager
 } // namespace state
