354428b299be65d89c9f7d88d67415a338da5618
Josh D. King <jdking@us.ibm.com>
2016-12-19 11:13:43 -0600

    Initial commit for BMC state management
    This is the starting code needing to
    build the application.  Still more to do,
    this is just the layout for the additional
    commits that will be done for the state
    management.
    
    Change-Id: Ief1dc96c84b34630e60ef40c99d4faf5b6f6e02e
    Signed-off-by: Josh D. King <jdking@us.ibm.com>

---
+++ BMC_state_manager_main.cpp
@@ -0,0 +1,25 @@
+#include "BMC_state_manager.hpp"
+#include "config.h"
+#include <sdbusplus/bus.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <exception>
+
+int main(int argc, char *argv[])
+{
+    auto bus = sdbusplus::bus::new_default();
+
+    auto manager = phosphor::state::manager::BMC(bus,
+                                                 BMC_BUSNAME,
+                                                 BMC_OBJPATH);
+    bus.request_name(BMC_BUSNAME);
+
+    while(true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+
+    exit(EXIT_SUCCESS);
+
+}
