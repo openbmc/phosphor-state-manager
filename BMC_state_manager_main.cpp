5319036fd12c7710eae28a447ecb46516b7cc8a0
Josh D. King <jdking@us.ibm.com>
2016-12-19 11:13:43 -0600

    Add base support for BMC state management
    This is the starting code needing to
    build the application.  Still more to do,
    this is just the layout for the additional
    commits that will be done for the state
    management.
    
    Change-Id: Ief1dc96c84b34630e60ef40c99d4faf5b6f6e02e
    Signed-off-by: Josh D. King <jdking@us.ibm.com>

---
+++ BMC_state_manager_main.cpp
@@ -0,0 +1,30 @@
+#include <sdbusplus/bus.hpp>
+#include "config.h"
+#include "BMC_state_manager.hpp"
+
+
+int main(int argc, char *argv[])
+{
+    auto bus = sdbusplus::bus::new_default();
+
+    // For now, we only have one instance of the BMC
+    // 0 is for the current instance
+    auto objPathInst = std::string(BMC_OBJPATH) + '0';
+
+    // Add sdbusplus ObjectManager.
+    sdbusplus::server::manager::manager objManager(bus, objPathInst.c_str());
+
+    phosphor::state::manager::BMC manager(bus,
+                                          objPathInst.c_str());
+
+    bus.request_name(BMC_BUSNAME);
+
+    while(true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+
+    exit(EXIT_SUCCESS);
+
+}
