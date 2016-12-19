bdd9cb7121ca36f29a199e35f8837ec73c7a4e20
Josh D. King <jdking@us.ibm.com>
2016-12-19 11:13:43 -0600

    Add base support for BMC state management
    This is the starting code needing to
    build the application.  Still more to do,
    this is just the layout for the additional
    commits that will be done for the state
    management.
    
    Change-Id: Ief1dc96c84b34630e60ef40c99d4faf5b6f6e02e
    Signed-off-by: Josh D. King <jdking@us.ibm.com>

---
+++ bmc_state_manager.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include "xyz/openbmc_project/State/BMC/server.hpp"
+
+namespace phosphor
+{
+namespace state
+{
+namespace manager
+{
+
+/** @class BMC
+ *  @brief OpenBMC BMC state management implementation.
+ *  @details A concrete implementation for xyz.openbmc_project.State.BMC
+ *  DBus API.
+ */
+class BMC : public sdbusplus::server::object::object<
+                       sdbusplus::xyz::openbmc_project::State::server::BMC>
+{
+    public:
+        /** @brief Constructs BMC State Manager
+         *
+         * @param[in] bus       - The Dbus bus object
+         * @param[in] busName   - The Dbus name to own
+         * @param[in] objPath   - The Dbus object path
+         */
+        BMC(sdbusplus::bus::bus& bus,
+            const char* objPath) :
+                sdbusplus::server::object::object<
+                    sdbusplus::xyz::openbmc_project::State::server::BMC>(
+                        bus, objPath) {};
+
+};
+
+} // namespace manager
+} // namespace state
+} // namespace phosphor
