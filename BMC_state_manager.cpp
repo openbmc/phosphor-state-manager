f595f519640441a3ccae2ddc72d1483050cd6ff6
Josh D. King <jdking@us.ibm.com>
2017-01-04 07:53:19 -0600

    Move tracing over to phosphor-logging interfaces
    Change-Id: I7ceffeadf44c048b4fe6d2510a14b8f4a50176fe
    Signed-off-by: Josh D. King <jdking@us.ibm.com>

--- BMC_state_manager.cpp
+++ BMC_state_manager.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <systemd/sd-bus.h>
+#include <log.hpp>
 #include "BMC_state_manager.hpp"
 
 namespace phosphor
@@ -11,6 +12,8 @@ namespace manager
 
 // When you see server:: you know we're referencing our base class
 namespace server = sdbusplus::xyz::openbmc_project::State::server;
+
+using namespace phosphor::logging;
 
 /* Map a transition to it's systemd target */
 const std::map<server::BMC::Transition,std::string> SYSTEMD_TABLE = {
@@ -37,12 +40,16 @@ void BMC::determineInitialState()
 
     if(sysState == "HOST_BOOTED")
     {
-        std::cout << "BMC is READY " << sysState << std::endl;
+        log<level::INFO>("Initial BMC State will be Ready",
+                         entry("CURRENT_BMC_STATE=0x%.2X",
+                               BMCState::Ready));
         currentBMCState(BMCState::Ready);
     }
     else
     {
-        std::cout << "BMC is not READY " << sysState << std::endl;
+        log<level::INFO>("Initial BMC State will be NotReady",
+                         entry("CURRENT_BMC_STATE=0x%.2X",
+                               BMCState::NotReady));
         currentBMCState(BMCState::NotReady);
     }
 
@@ -91,15 +98,21 @@ int BMC::handleSysStateChange(sd_bus_message *msg, void *usrData,
 
 BMC::Transition BMC::requestedBMCTransition(Transition value)
 {
-    std::cout << "Setting the RequestedBMCTransition field" << std::endl;
+    log<level::INFO>(
+            "Setting the RequestedBMCTransition field",
+            entry("REQUESTED_BMC_TRANSITION=0x%.2X", value));
     executeTransition(value);
-    std::cout << "......SUCCESS" << std::endl;
+    log<level::INFO>(
+            "...SUCCESS",
+            entry("REQUESTED_BMC_TRANSITION=0x%.2X", value));
     return server::BMC::requestedBMCTransition(value);
 }
 
 BMC::BMCState BMC::currentBMCState(BMCState value)
 {
-    std::cout << "Setting the BMCState field" << std::endl;
+    log<level::INFO>(
+            "Change to BMC State",
+            entry("CURRENT_BMC_STATE=0x%.2X", value));
     return server::BMC::currentBMCState(value);
 }
 
