59965ef167f25f30705fcc462ae270235e9c30f1
Josh D. King <jdking@us.ibm.com>
2017-03-02 10:58:11 -0600

    Remove Hosts power on messages when powering off
    Currently Host state services show incorrect states
    when a target exits. To fix this I did a bus call to
    the target to determine its ActiveState.  Doing that
    allows me to determine if the target is already
    powered on and is now powering down.
    
    Change-Id: Id91dc8156308372fc1649526a7b6dedee94eeb01
    Signed-off-by: Josh D. King <jdking@us.ibm.com>

--- host_state_manager.hpp
+++ host_state_manager.hpp
@@ -88,6 +88,16 @@ class Host : public sdbusplus::server::object::object<
         void executeTransition(Transition tranReq);
 
         /**
+         * @brief Determine if target is powering off
+         *
+         * This function determines if the target is powering off and
+         * helps prevent misleading log recorded states.
+         *
+         * @return boolean corresponding to host powering off
+         **/
+        bool isPoweringOff();
+
+        /**
          * @brief Determine if auto reboot flag is set
          *
          * @return boolean corresponding to current auto_reboot setting
