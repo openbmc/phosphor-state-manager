da9a9e4bf4279089fb188235e656bc1b816470a7
Josh D. King <jdking@us.ibm.com>
2017-03-02 11:15:55 -0600

    Remove Chassis power on messages when powering off
    Currently Chassis state services show incorrect states
    when a target exits. To fix this, a bus call is used on
    the target to determine its ActiveState.  Doing that
    helps to determine if the target is already
    powered on and is now powering down.
    
    Change-Id: I55bc87fa9ff647212fe545b7edd7df8a85e9e223
    Signed-off-by: Josh D. King <jdking@us.ibm.com>

--- chassis_state_manager.hpp
+++ chassis_state_manager.hpp
@@ -80,6 +80,18 @@ class Chassis : public sdbusplus::server::object::object<
          */
         void executeTransition(Transition tranReq);
 
+        /**
+         * @brief Determine if target is powered active
+         *
+         * This function determines if the target is active and
+         * helps prevent misleading log recorded states.
+         *
+         * @param[in] target - Target string to check on
+         *
+         * @return boolean corresponding to state active
+         **/
+        bool stateActive(const std::string& target);
+
         /** @brief Callback function on systemd state changes
          *
          * Will just do a call into the appropriate object for processing
