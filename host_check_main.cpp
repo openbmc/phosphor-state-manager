d1e3a557c942cd062fc7408d2078d289c013c2d9
Andrew Geissler <andrewg@us.ibm.com>
2017-03-24 15:55:17 -0500

    Initial skeleton of host check code
    This sequence of commits will be the application
    that runs after a BMC reset when it's detected that
    pgood is on.  This applications job is to check
    whether the host is running, and if it is, to
    create a specific file in the filesystem that
    will prevent the host power on targets services
    from executing.
    
    Change-Id: If5d26a443a0d8fd1873df166a6a5d45290eb6401
    Signed-off-by: Andrew Geissler <andrewg@us.ibm.com>

---
+++ host_check_main.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <sdbusplus/bus.hpp>
+#include <phosphor-logging/log.hpp>
+
+using namespace phosphor::logging;
+
+int main(int argc, char *argv[])
+{
+    log<level::INFO>("Check if host is running");
+
+    auto bus = sdbusplus::bus::new_default();
+
+    // Setup Signal Handler
+
+    // Initiate heartbeat command
+
+    // Wait for signal
+
+    // If host running then create file
+
+    return 0;
+}
