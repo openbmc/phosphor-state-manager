14657ba51ffc0994cce2956b06e5c40a2388c816
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2017-06-19 06:43:22 -0500

    Implementation of OS status, boot count and boot progress
    Change-Id: I1b00a932db2533b83fa0bd7bb4fbd62c0299bff7
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- host_state_manager_main.cpp
+++ host_state_manager_main.cpp
@@ -4,10 +4,20 @@
 #include <sdbusplus/bus.hpp>
 #include "config.h"
 #include "host_state_manager.hpp"
+#include <xyz/openbmc_project/State/Boot/Progress/server.hpp>
+#include <xyz/openbmc_project/Control/Boot/RebootAttempts/server.hpp>
+#include <xyz/openbmc_project/State/OperatingSystem/Status/server.hpp>
 
 int main(int argc, char *argv[])
 {
     auto bus = sdbusplus::bus::new_default();
+
+    using BootProgress = sdbusplus::xyz::openbmc_project::State::
+                         Boot::server::Progress;
+    using BootCount = sdbusplus::xyz::openbmc_project::Control::
+                         Boot::server::RebootAttempts;
+    using OsStatus = sdbusplus::xyz::openbmc_project::State::
+                         OperatingSystem::server::Status;
 
     // For now, we only have one instance of the host
     auto objPathInst = std::string{HOST_OBJPATH} + '0';
@@ -20,6 +30,21 @@ int main(int argc, char *argv[])
                                            objPathInst.c_str());
 
     bus.request_name(HOST_BUSNAME);
+
+    //Boot progress
+    BootProgress bootProgress(bus, BOOT_PROGRESS_OBJPATH);
+
+    //Boot Count
+    sdbusplus::server::manager::manager BootCountObjManager(bus,
+            BOOT_COUNT_OBJPATH);
+
+    BootCount bootCount(bus, BOOT_COUNT_OBJPATH);
+
+    //set boot count to initial default value
+    bootCount.attemptsLeft(BOOT_COUNT_MAX_ALLOWED);
+
+    //OperatingSystem Status
+    OsStatus osStatus(bus, OS_STATUS_OBJPATH);
 
     while(true)
     {
