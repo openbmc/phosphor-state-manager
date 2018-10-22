236c766aa792dc4a8ac684ebbd615890db2fc72a
Anthony Wilson <wilsonan@us.ibm.com>
2018-10-22 17:23:42 -0500

    utility: Implement convertGpioToNum function
    Change-Id: Ie97f336cbf18567d73137fcdd66bb38b67036ca5
    Signed-off-by: Anthony Wilson <wilsonan@us.ibm.com>

--- utility.cpp
+++ utility.cpp
@@ -16,8 +16,37 @@ namespace utility
 
 int convertGpioToNum(const char* gpio)
 {
-    /* TODO */
-    return 0;
+    size_t len = std::strlen(gpio);
+    if (len < 2)
+    {
+        fprintf(stderr, "Invald GPIO name %s\n", gpio);
+        return -1;
+    }
+
+    /* Read the offset from the last character */
+    if (!std::isdigit(gpio[len - 1]))
+    {
+        fprintf(stderr, "Invalid GPIO offset in GPIO %s\n", gpio);
+        return -1;
+    }
+
+    int offset = gpio[len - 1] - '0';
+
+    /* Read the port from the second to last character */
+    if (!std::isalpha(gpio[len - 2]))
+    {
+        fprintf(stderr, "Invalid GPIO port in GPIO %s\n", gpio);
+        return -1;
+    }
+    int port = std::toupper(gpio[len - 2]) - 'A';
+
+    /* Check for a 2 character port, like AA */
+    if ((len == 3) && std::isalpha(gpio[len - 3]))
+    {
+        port += 26 * (std::toupper(gpio[len - 3]) - 'A' + 1);
+    }
+
+    return (port * gpioPortOffset) + offset;
 }
 
 bool gpioSetValue(const char* gpioName, bool activeLow, bool asserted)
