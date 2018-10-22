72e9becad19cacf5a0e2c731c4aa4c1169238c4e
Anthony Wilson <wilsonan@us.ibm.com>
2018-10-22 17:38:58 -0500

    utility: Add unit testing for utility functions
    Unit tests for convertGpioToNum achieves code coverage.
    
    Unit tests for gpioSetValue are pending.
    
    Change-Id: I17d06407e8d5a43293e5c77d47b0d5559c03d3ec
    Signed-off-by: Anthony Wilson <wilsonan@us.ibm.com>

---
+++ test/test_utility.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "../utility.hpp"
+
+using namespace utility;
+
+TEST(UtilityTest, TestConvertGPIOToNum)
+{
+    /* len should be 2 or more */
+    EXPECT_EQ(convertGpioToNum(""), -1);
+
+    /* Last char should be a digit */
+    EXPECT_EQ(convertGpioToNum("AB"), -1);
+
+    /* First char should be alpha char*/
+    EXPECT_EQ(convertGpioToNum("1C"), -1);
+
+    /* Check correct 2 length string */
+    EXPECT_EQ(convertGpioToNum("D2"), 26);
+
+    /* Check correct 3 length string */
+    EXPECT_EQ(convertGpioToNum("EF3"), 1083);
+}
+
+TEST(UtilityTest, TestGPIOSetValue)
+{
+    /*TODO*/
+}
