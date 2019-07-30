7e7cb9d5df4ac9d50283c5483522b7f503005021
Andrew Geissler <geissonator@yahoo.com>
2019-07-30 14:08:17 -0500

    sysd_monitor: Initial application framework
    This sets the base for a sequence of commits which will enable the
    functionality defined within this design document:
    https://github.com/openbmc/docs/blob/master/designs/target-fail-monitoring.md
    
    All TODO comments will be complete by the end of this patch set
    sequence.
    
    Change-Id: Ifb263ad5e4db6f07fe0b1a4638fb052e5d8beaa5
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

---
+++ systemd_target_monitor.cpp
@@ -0,0 +1,22 @@
+#include <phosphor-logging/log.hpp>
+#include <sdbusplus/bus.hpp>
+#include <sdeventplus/event.hpp>
+
+using phosphor::logging::level;
+using phosphor::logging::log;
+
+int main(int argc, char* argv[])
+{
+    auto bus = sdbusplus::bus::new_default();
+    auto event = sdeventplus::Event::get_default();
+    bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+
+    // TODO - Process input parameters
+
+    // TODO - Load in json config file(s)
+
+    // TODO - Begin monitoring for systemd unit changes and logging appropriate
+    //        errors
+
+    return event.loop();
+}
