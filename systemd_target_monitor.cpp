52cd70c5dc32ccb0414b865d66dff6330aba7381
Andrew Geissler <geissonator@yahoo.com>
2019-07-30 15:16:20 -0500

    sysd_monitor: Parse command line parameters
    Tested:
    
    - Verified multiple -f and --file options
    - Verified if same file is put in that only one ends up in data
      structure
    - Verified -h and --help option
    
    Change-Id: I0b528c7534df051685991576c040d0650faabf21
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

--- systemd_target_monitor.cpp
+++ systemd_target_monitor.cpp
@@ -1,13 +1,35 @@
+#include <CLI/CLI.hpp>
+#include <iostream>
 #include <sdbusplus/bus.hpp>
 #include <sdeventplus/event.hpp>
+
+void print_usage(void)
+{
+    std::cout << "[-f <file1> -f <file2> ...] : Full path to json file(s) with "
+                 "target/error mappings"
+              << std::endl;
+    return;
+}
 
 int main(int argc, char* argv[])
 {
     auto bus = sdbusplus::bus::new_default();
     auto event = sdeventplus::Event::get_default();
     bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+    std::vector<std::string> filePaths;
 
-    // TODO - Process input parameters
+    CLI::App app{"OpenBmc systemd target monitor"};
+    app.add_option("-f,--file", filePaths,
+                   "Full path to json file(s) with target/error mappings");
+
+    CLI11_PARSE(app, argc, argv);
+
+    if (filePaths.empty())
+    {
+        log<level::ERR>("No input files");
+        print_usage();
+        exit(-1);
+    }
 
     // TODO - Load in json config file(s)
 
