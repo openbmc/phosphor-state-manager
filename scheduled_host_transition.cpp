52b94a2ec39f7f9fb9650ac86d8b283f53717b0f
Carol Wang <wangkair@cn.ibm.com>
2020-02-18 17:39:49 +0800

    sched-host-tran: add base interfaces for Scheduled Host Transition
    Implement interfaces to get/set scheduledTime and requestedTransition.
    
    Tested:
    1. Check scheduledTime
     # busctl get-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/host0 \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime
       t 0
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/host0 \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1
     # busctl get-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/host0 \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime
       t 1
    2. Check requestedTransition
     # busctl get-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/host0 \
       xyz.openbmc_project.State.ScheduledHostTransition RequestedTransition
       s "xyz.openbmc_project.State.Host.Transition.On"
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/host0 \
       xyz.openbmc_project.State.ScheduledHostTransition RequestedTransition \
       s "xyz.openbmc_project.State.Host.Transition.Off"
     # busctl get-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/host0 \
       xyz.openbmc_project.State.ScheduledHostTransition RequestedTransition
       s "xyz.openbmc_project.State.Host.Transition.Off"
    
    Change-Id: Ie7da8034d37c1069db043772f35982ca821826ae
    Signed-off-by: Carol Wang <wangkair@cn.ibm.com>

---
+++ scheduled_host_transition.cpp
@@ -0,0 +1,20 @@
+#include "scheduled_host_transition.hpp"
+
+namespace phosphor
+{
+namespace state
+{
+namespace manager
+{
+
+using HostTransition =
+    sdbusplus::xyz::openbmc_project::State::server::ScheduledHostTransition;
+
+uint64_t ScheduledHostTransition::scheduledTime(uint64_t value)
+{
+    return HostTransition::scheduledTime(value);
+}
+
+} // namespace manager
+} // namespace state
+} // namespace phosphor
