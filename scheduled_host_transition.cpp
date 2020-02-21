03594644f8cbf7793e8d8e3c5c8f0a86ce85a09d
Carol Wang <wangkair@cn.ibm.com>
2020-02-21 10:12:01 +0800

    SHT: implement host transition process
    Set the scheduled time and host transition to trigger power on/off.
    
    Tested:
    1. Check the state first
     $ curl -k -H "X-Auth-Token: $token" https://$bmc/xyz/openbmc_project/state/host0
     {
       "data": {
         "AttemptsLeft": 3,
         "BootProgress": "xyz.openbmc_project.State.Boot.Progress.ProgressStages.Unspecified",
         "CurrentHostState": "xyz.openbmc_project.State.Host.HostState.Off",
         "OperatingSystemState": "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive",
         "RequestedHostTransition": "xyz.openbmc_project.State.Host.Transition.Off"
       },
       "message": "200 OK",
       "status": "ok"
     }
    2. Set a time in future
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
        /xyz/openbmc_project/state/scheduledHostTransition \
        xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1582184830
     # busctl get-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/scheduledHostTransition \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime
       t 1582184830
    3. Check the state again after scheduled time
     Jan 15 06:38:20 WS-Seq-FW-2 phosphor-host-state-manager[442]: Host State transaction request
     $ curl -k -H "X-Auth-Token: $token" https://$bmc/xyz/openbmc_project/state/host0
     {
       "data": {
         "AttemptsLeft": 3,
         "BootProgress": "xyz.openbmc_project.State.Boot.Progress.ProgressStages.Unspecified",
         "CurrentHostState": "xyz.openbmc_project.State.Host.HostState.Running",
         "OperatingSystemState": "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive",
         "RequestedHostTransition": "xyz.openbmc_project.State.Host.Transition.On"
       },
       "message": "200 OK",
       "status": "ok"
     }
    4. Test when the host transition is on already
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/scheduledHostTransition \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1582250050
       Feb 21 01:54:10 witherspoon phosphor-scheduled-host-transition[6853]: \
       The state of host transition is as requestedTransition already.
    5. Set quested transition to off
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/scheduledHostTransition \
       xyz.openbmc_project.State.ScheduledHostTransition RequestedTransition \
       s "xyz.openbmc_project.State.Host.Transition.Off"
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
      /xyz/openbmc_project/state/scheduledHostTransition \
      xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1582250580
     $ curl -k -H "X-Auth-Token: $token" https://$bmc/xyz/openbmc_project/state/host0
     {
       "data": {
       "AttemptsLeft": 3,
       "BootProgress": "xyz.openbmc_project.State.Boot.Progress.ProgressStages.Unspecified",
       "CurrentHostState": "xyz.openbmc_project.State.Host.HostState.Off",
       "OperatingSystemState": "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive",
       "RequestedHostTransition": "xyz.openbmc_project.State.Host.Transition.Off"
       },
     "message": "200 OK",
     "status": "ok"
     }
    
    Change-Id: Ib9f3a3984005d9187a9b98603ec1598d8992869e
    Signed-off-by: Carol Wang <wangkair@cn.ibm.com>

--- scheduled_host_transition.cpp
+++ scheduled_host_transition.cpp
@@ -5,6 +5,7 @@
 #include <phosphor-logging/log.hpp>
 #include <xyz/openbmc_project/ScheduledTime/error.hpp>
 #include <chrono>
+#include <iostream>
 
 namespace phosphor
 {
@@ -18,15 +19,28 @@ using namespace phosphor::logging;
 using namespace xyz::openbmc_project::ScheduledTime;
 using FailedError =
     sdbusplus::xyz::openbmc_project::ScheduledTime::Error::InvalidTime;
+using sdbusplus::exception::SdBusError;
 using HostTransition =
     sdbusplus::xyz::openbmc_project::State::server::ScheduledHostTransition;
+
+constexpr auto MAPPER_BUSNAME = "xyz.openbmc_project.ObjectMapper";
+constexpr auto MAPPER_PATH = "/xyz/openbmc_project/object_mapper";
+constexpr auto MAPPER_INTERFACE = "xyz.openbmc_project.ObjectMapper";
+constexpr auto PROPERTY_INTERFACE = "org.freedesktop.DBus.Properties";
 
 uint64_t ScheduledHostTransition::scheduledTime(uint64_t value)
 {
     if (value == 0)
     {
         // 0 means the function Scheduled Host Transition is disabled
-        // to do: check timer, stop timer
+        // Stop the timer if it's running
+        if (timer.isEnabled())
+        {
+            timer.setEnabled(false);
+        }
+        // Set scheduledTime to 0 to disable host transition
+        HostTransition::scheduledTime(0);
+
         log<level::INFO>("The function Scheduled Host Transition is disabled.");
         return HostTransition::scheduledTime(value);
     }
@@ -34,13 +48,16 @@ uint64_t ScheduledHostTransition::scheduledTime(uint64_t value)
     auto deltaTime = seconds(value) - getTime();
     if (deltaTime < seconds(0))
     {
+        // Set scheduledTime to 0 to disable host transition
+        HostTransition::scheduledTime(0);
+
         log<level::ERR>("Scheduled time is earlier than current time. Fail to "
                         "do host transition.");
         elog<FailedError>(InvalidTime::REASON("Scheduled time is in the past"));
     }
     else
     {
-        // start timer
+        timer.restart(deltaTime);
     }
     return HostTransition::scheduledTime(value);
 }
@@ -49,6 +66,121 @@ seconds ScheduledHostTransition::getTime()
 {
     auto now = system_clock::now();
     return duration_cast<seconds>(now.time_since_epoch());
+}
+
+std::string getService(sdbusplus::bus::bus& bus, std::string path,
+                       std::string interface)
+{
+    auto mapper = bus.new_method_call(MAPPER_BUSNAME, MAPPER_PATH,
+                                      MAPPER_INTERFACE, "GetObject");
+
+    mapper.append(path, std::vector<std::string>({interface}));
+
+    std::map<std::string, std::vector<std::string>> mapperResponse;
+    try
+    {
+        auto mapperResponseMsg = bus.call(mapper);
+
+        mapperResponseMsg.read(mapperResponse);
+        if (mapperResponse.empty())
+        {
+            log<level::ERR>("Error reading mapper response",
+                            entry("PATH=%s", path.c_str()),
+                            entry("INTERFACE=%s", interface.c_str()));
+            throw std::runtime_error("Error reading mapper response");
+        }
+    }
+    catch (const SdBusError& e)
+    {
+        log<level::ERR>("Error in mapper call", entry("ERROR=%s", e.what()),
+                        entry("PATH=%s", path.c_str()),
+                        entry("INTERFACE=%s", interface.c_str()));
+        throw;
+    }
+
+    return mapperResponse.begin()->first;
+}
+
+std::string getProperty(sdbusplus::bus::bus& bus, std::string path,
+                        std::string interface, std::string propertyName)
+{
+    sdbusplus::message::variant<std::string> property;
+    std::string service = getService(bus, path, interface);
+
+    auto method = bus.new_method_call(service.c_str(), path.c_str(),
+                                      PROPERTY_INTERFACE, "Get");
+
+    method.append(interface, propertyName);
+
+    try
+    {
+        auto reply = bus.call(method);
+        reply.read(property);
+    }
+    catch (const SdBusError& e)
+    {
+        log<level::ERR>("Error in property Get", entry("ERROR=%s", e.what()),
+                        entry("PROPERTY=%s", propertyName.c_str()));
+        throw;
+    }
+
+    if (sdbusplus::message::variant_ns::get<std::string>(property).empty())
+    {
+        log<level::ERR>("Error reading property response",
+                        entry("PROPERTY=%s", propertyName.c_str()));
+        throw std::runtime_error("Error reading property response");
+    }
+
+    return sdbusplus::message::variant_ns::get<std::string>(property);
+}
+
+void setProperty(sdbusplus::bus::bus& bus, const std::string& path,
+                 const std::string& interface, const std::string& property,
+                 const std::string& value)
+{
+    sdbusplus::message::variant<std::string> variantValue = value;
+    std::string service = getService(bus, path, interface);
+
+    auto method = bus.new_method_call(service.c_str(), path.c_str(),
+                                      PROPERTY_INTERFACE, "Set");
+
+    method.append(interface, property, variantValue);
+    bus.call_noreply(method);
+
+    return;
+}
+
+void ScheduledHostTransition::hostTransition()
+{
+    std::string hostPath = "/xyz/openbmc_project/state/host0";
+
+    // Scheduled Host Transition only support two states: On and Off
+    std::string reqState =
+        (HostTransition::requestedTransition() == Transition::On) ? "On"
+                                                                  : "Off";
+    auto reqTrans = "xyz.openbmc_project.State.Host.Transition." + reqState;
+    auto currTrans =
+        getProperty(bus, hostPath, HOST_BUSNAME, "RequestedHostTransition");
+
+    if (reqTrans != currTrans)
+    {
+        setProperty(bus, hostPath, HOST_BUSNAME, "RequestedHostTransition",
+                    reqTrans);
+    }
+    else
+    {
+        log<level::INFO>(
+            "The state of host transition is as requestedTransition already.");
+    }
+}
+
+void ScheduledHostTransition::callback()
+{
+    hostTransition();
+    // Stop timer, since we need to do host transition once only
+    timer.setEnabled(false);
+    // Set scheduledTime to 0 to disable host transition
+    HostTransition::scheduledTime(0);
 }
 
 } // namespace manager
