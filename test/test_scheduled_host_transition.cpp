bdc79ac9b341d3b28ecf2cddb1da2b3d23fe93d8
Carol Wang <wangkair@cn.ibm.com>
2020-02-21 10:12:01 +0800

    SHT: implement host transition process
    Set the scheduled time and host transition to trigger power on/off.
    
    Tested:
    1. Check the state first
     $ curl -k -H "X-Auth-Token: $token" https://$bmc/xyz/openbmc_project/state/host0
     {
       "data": {
         "AttemptsLeft": 3,
         "BootProgress": "xyz.openbmc_project.State.Boot.Progress.ProgressStages.Unspecified",
         "CurrentHostState": "xyz.openbmc_project.State.Host.HostState.Off",
         "OperatingSystemState": "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive",
         "RequestedHostTransition": "xyz.openbmc_project.State.Host.Transition.Off"
       },
       "message": "200 OK",
       "status": "ok"
     }
    2. Set a time in future
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
        /xyz/openbmc_project/state/scheduledHostTransition \
        xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1582184830
     # busctl get-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/scheduledHostTransition \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime
       t 1582184830
    3. Check the state again after scheduled time
     Jan 15 06:38:20 WS-Seq-FW-2 phosphor-host-state-manager[442]: Host State transaction request
     $ curl -k -H "X-Auth-Token: $token" https://$bmc/xyz/openbmc_project/state/host0
     {
       "data": {
         "AttemptsLeft": 3,
         "BootProgress": "xyz.openbmc_project.State.Boot.Progress.ProgressStages.Unspecified",
         "CurrentHostState": "xyz.openbmc_project.State.Host.HostState.Running",
         "OperatingSystemState": "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive",
         "RequestedHostTransition": "xyz.openbmc_project.State.Host.Transition.On"
       },
       "message": "200 OK",
       "status": "ok"
     }
    4. Test when the host transition is on already
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/scheduledHostTransition \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1582250050
       Feb 21 01:54:10 witherspoon phosphor-scheduled-host-transition[6853]: \
       The state of host transition is as requestedTransition already.
    5. Set quested transition to off
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/scheduledHostTransition \
       xyz.openbmc_project.State.ScheduledHostTransition RequestedTransition \
       s "xyz.openbmc_project.State.Host.Transition.Off"
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
      /xyz/openbmc_project/state/scheduledHostTransition \
      xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1582250580
     $ curl -k -H "X-Auth-Token: $token" https://$bmc/xyz/openbmc_project/state/host0
     {
       "data": {
       "AttemptsLeft": 3,
       "BootProgress": "xyz.openbmc_project.State.Boot.Progress.ProgressStages.Unspecified",
       "CurrentHostState": "xyz.openbmc_project.State.Host.HostState.Off",
       "OperatingSystemState": "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive",
       "RequestedHostTransition": "xyz.openbmc_project.State.Host.Transition.Off"
       },
     "message": "200 OK",
     "status": "ok"
     }
    
    Change-Id: Ib9f3a3984005d9187a9b98603ec1598d8992869e
    Signed-off-by: Carol Wang <wangkair@cn.ibm.com>

--- test/test_scheduled_host_transition.cpp
+++ test/test_scheduled_host_transition.cpp
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/test/sdbus_mock.hpp>
+#include <sdeventplus/event.hpp>
 #include <xyz/openbmc_project/ScheduledTime/error.hpp>
 
 namespace phosphor
@@ -16,15 +17,20 @@ namespace manager
 using namespace std::chrono;
 using FailedError =
     sdbusplus::xyz::openbmc_project::ScheduledTime::Error::InvalidTime;
+using HostTransition =
+    sdbusplus::xyz::openbmc_project::State::server::ScheduledHostTransition;
 
 class TestScheduledHostTransition : public testing::Test
 {
   public:
+    sdeventplus::Event event;
     sdbusplus::SdBusMock sdbusMock;
     sdbusplus::bus::bus mockedBus = sdbusplus::get_mocked_new(&sdbusMock);
     ScheduledHostTransition scheduledHostTransition;
 
-    TestScheduledHostTransition() : scheduledHostTransition(mockedBus, "")
+    TestScheduledHostTransition() :
+        event(sdeventplus::Event::get_default()),
+        scheduledHostTransition(mockedBus, "", event)
     {
         // Empty
     }
@@ -33,11 +39,22 @@ class TestScheduledHostTransition : public testing::Test
     {
         return scheduledHostTransition.getTime();
     }
+
+    bool getTimerStatus()
+    {
+        return scheduledHostTransition.timer.isEnabled();
+    }
+
+    void hostTransition()
+    {
+        scheduledHostTransition.hostTransition();
+    }
 };
 
 TEST_F(TestScheduledHostTransition, disableHostTransition)
 {
     EXPECT_EQ(scheduledHostTransition.scheduledTime(0), 0);
+    EXPECT_FALSE(getTimerStatus());
 }
 
 TEST_F(TestScheduledHostTransition, invalidScheduledTime)
@@ -45,6 +62,24 @@ TEST_F(TestScheduledHostTransition, invalidScheduledTime)
     // scheduled time is 1 min earlier than current time
     uint64_t schTime = (uint64_t)(getCurrentTime() - seconds(60)).count();
     EXPECT_THROW(scheduledHostTransition.scheduledTime(schTime), FailedError);
+}
+
+TEST_F(TestScheduledHostTransition, validScheduledTime)
+{
+    // scheduled time is 1 min later than current time
+    uint64_t schTime = (uint64_t)(getCurrentTime() + seconds(60)).count();
+    EXPECT_EQ(scheduledHostTransition.scheduledTime(schTime), schTime);
+    EXPECT_TRUE(getTimerStatus());
+}
+
+TEST_F(TestScheduledHostTransition, hostTransitionStatus)
+{
+    // set requested transition to be on
+    scheduledHostTransition.requestedTransition(Transition::On);
+    EXPECT_EQ(scheduledHostTransition.requestedTransition(), Transition::On);
+    // set requested transition to be off
+    scheduledHostTransition.requestedTransition(Transition::Off);
+    EXPECT_EQ(scheduledHostTransition.requestedTransition(), Transition::Off);
 }
 
 } // namespace manager
