dda7acd5b052f325e16384fdeb04222c14ade30f
Carol Wang <wangkair@cn.ibm.com>
2020-02-21 10:12:01 +0800

    SHT: implement host transition process
    Set the scheduled time and host transition to trigger power on/off.
    
    Tested:
    1. Check the state first
     $ curl -k -H "X-Auth-Token: $token" https://$bmc/xyz/openbmc_project/state/host0
     {
       "data": {
         "AttemptsLeft": 3,
         "BootProgress": "xyz.openbmc_project.State.Boot.Progress.ProgressStages.Unspecified",
         "CurrentHostState": "xyz.openbmc_project.State.Host.HostState.Off",
         "OperatingSystemState": "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive",
         "RequestedHostTransition": "xyz.openbmc_project.State.Host.Transition.Off"
       },
       "message": "200 OK",
       "status": "ok"
     }
    2. Set a time in future
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
        /xyz/openbmc_project/state/scheduledHostTransition \
        xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1582184830
     # busctl get-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/scheduledHostTransition \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime
       t 1582184830
    3. Check the state again after scheduled time
     Jan 15 06:38:20 WS-Seq-FW-2 phosphor-host-state-manager[442]: Host State transaction request
     $ curl -k -H "X-Auth-Token: $token" https://$bmc/xyz/openbmc_project/state/host0
     {
       "data": {
         "AttemptsLeft": 3,
         "BootProgress": "xyz.openbmc_project.State.Boot.Progress.ProgressStages.Unspecified",
         "CurrentHostState": "xyz.openbmc_project.State.Host.HostState.Running",
         "OperatingSystemState": "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive",
         "RequestedHostTransition": "xyz.openbmc_project.State.Host.Transition.On"
       },
       "message": "200 OK",
       "status": "ok"
     }
    4. Test when the host transition is on already
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/scheduledHostTransition \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1582250050
       Feb 21 01:54:10 witherspoon phosphor-scheduled-host-transition[6853]: \
       The state of host transition is as requestedTransition already.
    5. Set quested transition to off
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/scheduledHostTransition \
       xyz.openbmc_project.State.ScheduledHostTransition RequestedTransition \
       s "xyz.openbmc_project.State.Host.Transition.Off"
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
      /xyz/openbmc_project/state/scheduledHostTransition \
      xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1582250580
     $ curl -k -H "X-Auth-Token: $token" https://$bmc/xyz/openbmc_project/state/host0
     {
       "data": {
       "AttemptsLeft": 3,
       "BootProgress": "xyz.openbmc_project.State.Boot.Progress.ProgressStages.Unspecified",
       "CurrentHostState": "xyz.openbmc_project.State.Host.HostState.Off",
       "OperatingSystemState": "xyz.openbmc_project.State.OperatingSystem.Status.OSStatus.Inactive",
       "RequestedHostTransition": "xyz.openbmc_project.State.Host.Transition.Off"
       },
     "message": "200 OK",
     "status": "ok"
     }
    
    Change-Id: Ib9f3a3984005d9187a9b98603ec1598d8992869e
    Signed-off-by: Carol Wang <wangkair@cn.ibm.com>

--- scheduled_host_transition.hpp
+++ scheduled_host_transition.hpp
@@ -2,7 +2,10 @@
 
 #include <sdbusplus/bus.hpp>
 #include <phosphor-logging/log.hpp>
+#include <sdeventplus/event.hpp>
+#include <sdeventplus/utility/timer.hpp>
 #include <xyz/openbmc_project/State/ScheduledHostTransition/server.hpp>
+#include "config.h"
 
 namespace phosphor
 {
@@ -24,14 +27,17 @@ using ScheduledHostTransitionInherit = sdbusplus::server::object::object<
 class ScheduledHostTransition : public ScheduledHostTransitionInherit
 {
   public:
-    ScheduledHostTransition(sdbusplus::bus::bus& bus, const char* objPath) :
-        ScheduledHostTransitionInherit(bus, objPath, true)
+    ScheduledHostTransition(sdbusplus::bus::bus& bus, const char* objPath,
+                            const sdeventplus::Event& event) :
+        ScheduledHostTransitionInherit(bus, objPath, true),
+        bus(bus), event(event),
+        timer(event, std::bind(&ScheduledHostTransition::callback, this))
     {
     }
     ~ScheduledHostTransition() = default;
 
     /**
-     * @brief Set value of transition time
+     * @brief Hanlde with scheduled time
      *
      * @param[in] value - The seconds since UTC
      * @return The updated time for transition since UTC
@@ -39,11 +45,30 @@ class ScheduledHostTransition : public ScheduledHostTransitionInherit
     uint64_t scheduledTime(uint64_t value) override;
 
   private:
+    /** @brief sdbusplus bus client connection */
+    sdbusplus::bus::bus& bus;
+
+    /** @brief sdbusplus event */
+    const sdeventplus::Event& event;
+
     /** @brief Get current time
      *
      *  @return - return current epoch time
      */
     std::chrono::seconds getTime();
+
+    /** @brief Implement host transition
+     *
+     *  @return - Does not return anything. Error will result in exception
+     *            being thrown
+     */
+    void hostTransition();
+
+    /** @brief Used by the timer to do host transition */
+    void callback();
+
+    /** @brief Timer used for host transition with seconds*/
+    sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic> timer;
 };
 } // namespace manager
 } // namespace state
