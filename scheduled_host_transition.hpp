31a6f897536ba11f28b682662b2563b9d67111ef
Carol Wang <wangkair@cn.ibm.com>
2020-02-25 16:19:34 +0800

    sched-host-tran: handle with BMC time changing
    Handle with different processes when BMC time is changed after scheduled time
    is set.
    
    Tested:
    Case1: BMC time is changed to be later than current time but still earlier than
    scheduled time
    1. Get current time
     # date
       Tue Feb 25 07:07:44 UTC 2020
     # date +%s
       1582614271
    2. Schedule time, do host transition after at 07:20:00 around
     # busctl get-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/host0 \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime
       t 1582615256
    3. Change BMC time to 07:19:00 around
     # busctl set-property xyz.openbmc_project.Time.Manager \
       /xyz/openbmc_project/time/bmc xyz.openbmc_project.Time.EpochTime Elapsed \
       t 1582615136000000
     # date
       Tue Feb 25 07:19:20 UTC 2020
     # date +%s
       1582615187
    4. Host transition is done after 1 minute around,
       instead of waiting 13 mins around.
    
    Case2: BMC time is changed after scheduled time is reached
    Following Case1, the scheduled time is reached already,
    1. Change BMC time to 07:10:00 around
     # busctl set-property xyz.openbmc_project.Time.Manager \
       /xyz/openbmc_project/time/bmc xyz.openbmc_project.Time.EpochTime Elapsed \
       t 1582614600000000
    2. APP shows "The function Scheduled Host Transition is disabled", because
       the scheduled time is reached already and the scheduled time has been set
       to 0 after host transition is triggered.
    
    Case3: BMC time is changed to be bigger than scheduled time before scheduled
    time is reached
    1. Set scheduled time 07:08:00 around
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/host0 \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1582787314
    2. Change BMC time to 07:10:00 around
     # busctl set-property xyz.openbmc_project.Time.Manager \
       /xyz/openbmc_project/time/bmc xyz.openbmc_project.Time.EpochTime Elapsed \
       t 1582787434000000
    3. If the requested transition is the same as current host transition, it shows:
       "The state of host transition is as requestedTransition already".
    4. If the requested transition is not the same as current host transition,
       it will do host transition as requested.
    
    Case4: BMC time is changed to be earlier than current time
    1. Set scheduled time 07:10:00 around
     # busctl set-property xyz.openbmc_project.State.ScheduledHostTransition \
       /xyz/openbmc_project/state/host0 \
       xyz.openbmc_project.State.ScheduledHostTransition ScheduledTime t 1582787434
    2. Change BMC time to 07:08:00 around
     # busctl set-property xyz.openbmc_project.Time.Manager \
       /xyz/openbmc_project/time/bmc xyz.openbmc_project.Time.EpochTime Elapsed \
       t 1582787314000000
    3. App will wait 2 minutes more to do host transition.
    
    Change-Id: I23228be944d1b2f71161317228c8b16d7f5ca4eb
    Signed-off-by: Carol Wang <wangkair@cn.ibm.com>

--- scheduled_host_transition.hpp
+++ scheduled_host_transition.hpp
@@ -35,9 +35,10 @@ class ScheduledHostTransition : public ScheduledHostTransitionInherit
         bus(bus), event(event),
         timer(event, std::bind(&ScheduledHostTransition::callback, this))
     {
+        initialize();
     }
 
-    ~ScheduledHostTransition() = default;
+    ~ScheduledHostTransition();
 
     /**
      * @brief Handle with scheduled time
@@ -76,6 +77,39 @@ class ScheduledHostTransition : public ScheduledHostTransitionInherit
 
     /** @brief Timer used for host transition with seconds*/
     sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic> timer;
+
+    /** @brief The fd for time change event */
+    int timeFd = -1;
+
+    /** @brief Initialize timerFd related resource */
+    void initialize();
+
+    /** @brief The callback function on system time change
+     *
+     * @param[in] es - Source of the event
+     * @param[in] fd - File descriptor of the timer
+     * @param[in] revents - Not used
+     * @param[in] userdata - User data pointer
+     */
+    static int onTimeChange(sd_event_source* es, int fd, uint32_t revents,
+                            void* userdata);
+
+    /** @brief The deleter of sd_event_source */
+    std::function<void(sd_event_source*)> sdEventSourceDeleter =
+        [](sd_event_source* p) {
+            if (p)
+            {
+                sd_event_source_unref(p);
+            }
+        };
+    using SdEventSource =
+        std::unique_ptr<sd_event_source, decltype(sdEventSourceDeleter)>;
+
+    /** @brief The event source on system time change */
+    SdEventSource timeChangeEventSource{nullptr, sdEventSourceDeleter};
+
+    /** @brief Notified on bmc time is changed*/
+    void onBmcTimeChanged();
 };
 } // namespace manager
 } // namespace state
