da3d5738bb0da9057b0304f6fd322ee4a6a155ec
Andrew Geissler <geissonator@yahoo.com>
2021-02-09 15:11:10 -0600

    hypervisor: monitor boot progress
    The BootProgress property will be tracked to set the hypervisor state as
    appropriate
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>
    Change-Id: I5aa5d95893222169bc5abed6b851e795f2ad5ff0

---
+++ test/hypervisor_state.cpp
@@ -0,0 +1,41 @@
+#include "config.h"
+
+#include <hypervisor_state_manager.hpp>
+#include <sdbusplus/bus.hpp>
+#include <sdeventplus/event.hpp>
+
+#include <gtest/gtest.h>
+
+namespace server = sdbusplus::xyz::openbmc_project::State::server;
+
+TEST(checkBootProgress, BasicPaths)
+{
+    auto bus = sdbusplus::bus::new_default();
+    auto event = sdeventplus::Event::get_default();
+    bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+    auto objPathInst = std::string{HYPERVISOR_OBJPATH} + '0';
+
+    phosphor::state::manager::Hypervisor hypObj(bus, objPathInst.c_str());
+
+    std::string bootProgress = "Invalid.Boot.Progress";
+    hypObj.checkBootProgress(bootProgress);
+    EXPECT_EQ(hypObj.currentHostState(), server::Host::HostState::Off);
+
+    // TODO - Update test once Standby state is in for Host
+    bootProgress = "xyz.openbmc_project.State.Boot.Progress."
+                   "ProgressStages.SystemInitComplete";
+    hypObj.checkBootProgress(bootProgress);
+    EXPECT_EQ(hypObj.currentHostState(),
+              server::Host::HostState::TransitioningToOff);
+
+    bootProgress = "xyz.openbmc_project.State.Boot.Progress."
+                   "ProgressStages.OSStart";
+    hypObj.checkBootProgress(bootProgress);
+    EXPECT_EQ(hypObj.currentHostState(),
+              server::Host::HostState::TransitioningToRunning);
+
+    bootProgress = "xyz.openbmc_project.State.Boot.Progress."
+                   "ProgressStages.OSRunning";
+    hypObj.checkBootProgress(bootProgress);
+    EXPECT_EQ(hypObj.currentHostState(), server::Host::HostState::Running);
+}
