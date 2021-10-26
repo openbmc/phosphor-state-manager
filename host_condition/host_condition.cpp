54e0875f13a17acf500f999b008a908f27719421
Thang Tran <thuutran@amperecomputing.com>
2021-10-26 17:47:09 +0700

    Support checking host status via GPIO pins
    Currently, openBmc supports checking host status via ipmi and PLDM
    method. However, not all of platforms support IPMI and PLDM. Some
    platforms, like Ampere Mt.Jade checks input GPIO to detect Host
    firmware boot status. This commit supports Dbus method to check host
    status via GPIO pins, it checks the value of "host0-ready/-n" pin to
    detect HOST is ready or not.
    
    Signed-off-by: Thang Tran <thuutran@amperecomputing.com>
    Change-Id: I20e38e76c5b70119f0c86e5b497d47453d7c5a6c

---
+++ host_condition/host_condition.cpp
@@ -0,0 +1,54 @@
+#include "host_condition.hpp"
+#include <gpiod.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+
+namespace phosphor
+{
+namespace condition
+{
+Host::FirmwareCondition Host::currentFirmwareCondition() const
+{
+    bool isActHigh = true;
+    bool foundFlag = false;
+    int32_t gpioVal = 0;
+    std::string lineName = "host0-ready";
+    auto retVal = Host::FirmwareCondition::Unknown;
+
+    /*
+     * Check the host0-ready/-n pin is defined or not
+     */
+    if( gpiod::find_line(lineName) )
+    {
+        foundFlag = true;
+    }
+    else if( gpiod::find_line(lineName + "-n") )
+    {
+        lineName += "-n";
+        foundFlag = true;
+        isActHigh = false;
+    }
+
+    /*
+     * Check host is ready or not
+     */
+    if( true == foundFlag )
+    {
+        auto line = gpiod::find_line(lineName);
+
+        line.request({lineName, gpiod::line_request::DIRECTION_INPUT,
+            (true == isActHigh) ? 0 : gpiod::line_request::FLAG_ACTIVE_LOW});
+
+        gpioVal = line.get_value();
+        line.release();
+
+        retVal = (0 == gpioVal) ? Host::FirmwareCondition::Off : 
+                                Host::FirmwareCondition::Running;
+    }
+
+    return retVal;
+}
+
+} // namespace dbus_api
+} // namespace gpio
+
---
+++ host_condition/host_condition_main.cpp
@@ -0,0 +1,38 @@
+#include "config.h"
+#include "host_condition.hpp"
+#include <sdbusplus/bus.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/asio/io_context.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/bus.hpp>
+
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+    sd_bus* bus;
+    // Connect to system bus
+    auto io = std::make_shared<boost::asio::io_context>();
+    std::string objGroupName = HOST_GPIOS_OBJPATH;
+    std::string objPathInst = objGroupName + "/host0";
+    std::string busName = HOST_GPIOS_BUSNAME;
+
+    sd_bus_default_system(&bus);
+
+    auto sdbusp = std::make_shared<sdbusplus::asio::connection>(*io, bus);
+
+    auto objManager = std::make_unique<sdbusplus::server::manager::manager>(
+        *sdbusp, objGroupName.c_str());
+
+    auto host = std::make_unique<phosphor::condition::Host>(*sdbusp,
+                                                           objPathInst.c_str());
+
+    sdbusp->request_name(busName.c_str());
+
+    io->run();
+
+    return 0;
+}
+
