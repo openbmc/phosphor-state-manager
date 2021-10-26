3c2f984bd9bf18b556c052d74dcef12259826cef
Thang Tran <thuutran@amperecomputing.com>
2021-10-26 17:47:09 +0700

    Support checking host status via GPIO pins
    Currently, openBmc supports checking host status via ipmi and PLDM
    method. However, not all of platforms support IPMI and PLDM. Some
    platforms, like Ampere Mt.Jade checks input GPIO to detect Host
    firmware boot status. This commit supports Dbus method to check host
    status via GPIO pins, it checks the value of "host0-ready/-n" pin to
    detect HOST is ready or not.
    
    Signed-off-by: Thang Tran <thuutran@amperecomputing.com>
    Change-Id: I20e38e76c5b70119f0c86e5b497d47453d7c5a6c

---
+++ host_condition/host_condition.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <xyz/openbmc_project/Condition/HostFirmware/server.hpp>
+
+#include <iostream>
+
+namespace phosphor
+{
+namespace condition
+{
+
+using HostIntf = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Condition::server::HostFirmware>;
+
+class Host : public HostIntf
+{
+  public:
+    Host() = delete;
+    Host(const Host&) = delete;
+    Host& operator=(const Host&) = delete;
+    Host(Host&&) = delete;
+    Host& operator=(Host&&) = delete;
+    virtual ~Host() = default;
+
+    Host(sdbusplus::bus::bus& bus, const std::string& path) :
+        HostIntf(bus, path.c_str()){
+            /* Do nothing */
+        };
+
+    /** @brief Override reads to CurrentFirmwareCondition */
+    FirmwareCondition currentFirmwareCondition() const override;
+};
+} // namespace dbus_api
+} // namespace gpio
+
