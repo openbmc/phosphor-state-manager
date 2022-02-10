de5404782457551547cfccf8d93b48149467c3d3
Allen.Wang <Allen_Wang@quantatw.com>
2022-02-10 17:59:20 +0800

    host-state-manager: Add multi-host support
    Add support management multiple host state with multi process.
    Each process obtain a d-bus object for corresponding host.
    
    TESTED : Built the openbmc image for Facebook Bletchley hardware.
    Verified Host State buses/objects created successfully
    
    root@bletchley:~# busctl tree xyz.openbmc_project.State.Host1
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/state
          └─/xyz/openbmc_project/state/host1
    root@bletchley:~# busctl tree xyz.openbmc_project.State.Host2
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/state
          └─/xyz/openbmc_project/state/host2
    ...
    
    Signed-off-by: Allen.Wang <Allen_Wang@quantatw.com>
    Change-Id: Ie18007122a5df9e33f387e691eaa9979ce18ed0e

--- host_state_manager.hpp
+++ host_state_manager.hpp
@@ -52,7 +52,7 @@ class Host : public HostInherit
      * @param[in] bus       - The Dbus bus object
      * @param[in] objPath   - The Dbus object path
      */
-    Host(sdbusplus::bus::bus& bus, const char* objPath) :
+    Host(sdbusplus::bus::bus& bus, const char* objPath, const std::string& hostId) :
         HostInherit(bus, objPath, true), bus(bus),
         systemdSignalJobRemoved(
             bus,
@@ -68,10 +68,13 @@ class Host : public HostInherit
                 sdbusRule::interface("org.freedesktop.systemd1.Manager"),
             std::bind(std::mem_fn(&Host::sysStateChangeJobNew), this,
                       std::placeholders::_1)),
-        settings(bus)
+        settings(bus),
+        hostId(hostId)
     {
         // Enable systemd signals
         subscribeToSystemdSignals();
+
+        createSystemdTargets();
 
         // Will throw exception on fail
         determineInitialState();
@@ -134,6 +137,11 @@ class Host : public HostInherit
      * @return Will throw exceptions on failure
      **/
     void determineInitialState();
+
+    /**
+     * create systemd target instance names and mapping table
+     **/
+    void createSystemdTargets();
 
     /** @brief Execute the transition request
      *
@@ -278,6 +286,8 @@ class Host : public HostInherit
 
     // Settings objects of interest
     settings::Objects settings;
+
+    std::string hostId;
 };
 
 } // namespace manager
