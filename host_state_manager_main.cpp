d72e456bdbb746321be3f4493a35dcaa325286a9
Allen.Wang <Allen_Wang@quantatw.com>
2022-02-10 17:59:20 +0800

    host-state-manager: Add multi-host support
    Add support management multiple host state with multi process.
    Each process obtain a d-bus object for corresponding host.
    
    TESTED : Built the openbmc image for Facebook Bletchley hardware.
    Verified Host State buses/objects created successfully
    
    root@bletchley:~# busctl tree xyz.openbmc_project.State.Host1
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/state
          └─/xyz/openbmc_project/state/host1
    root@bletchley:~# busctl tree xyz.openbmc_project.State.Host2
    └─/xyz
      └─/xyz/openbmc_project
        └─/xyz/openbmc_project/state
          └─/xyz/openbmc_project/state/host2
    ...
    
    Signed-off-by: Allen.Wang <Allen_Wang@quantatw.com>
    Change-Id: Ie18007122a5df9e33f387e691eaa9979ce18ed0e

--- host_state_manager_main.cpp
+++ host_state_manager_main.cpp
@@ -9,24 +9,34 @@
 #include <experimental/filesystem>
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
+    std::string hostId = "0";
+    std::string hostBusName = HOST_BUSNAME;
+    if (argc == 2)
+    {
+        hostId = argv[1];
+        if (hostId != "0")
+        {
+            hostBusName += hostId;
+        }
+    }
+
     namespace fs = std::experimental::filesystem;
 
     auto bus = sdbusplus::bus::new_default();
 
-    // For now, we only have one instance of the host
-    auto objPathInst = std::string{HOST_OBJPATH} + '0';
+    auto objPathInst = std::string{HOST_OBJPATH} + hostId;
 
     // Add sdbusplus ObjectManager.
     sdbusplus::server::manager::manager objManager(bus, objPathInst.c_str());
 
-    phosphor::state::manager::Host manager(bus, objPathInst.c_str());
+    phosphor::state::manager::Host manager(bus, objPathInst.c_str(), hostId);
 
     auto dir = fs::path(HOST_STATE_PERSIST_PATH).parent_path();
     fs::create_directories(dir);
 
-    bus.request_name(HOST_BUSNAME);
+    bus.request_name(hostBusName.c_str());
 
     while (true)
     {
