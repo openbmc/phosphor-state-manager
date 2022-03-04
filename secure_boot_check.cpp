8d8d731d6a638331527c64f6a8e3fa3c830ba04c
Andrew Geissler <geissonator@yahoo.com>
2022-03-04 14:42:26 -0600

    secure-boot: check sysfs debug settings
    These aspeed-specific settings are provided via the kernels sysfs
    filesystem. They provide information on the security of the BMC.
    
    Follow on commits will check if the BMC is in manufacturing mode and log
    an error if the system does not pass the security check.
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>
    Change-Id: Iddcc09dc34133806abe33570c5422bde51981bdb

--- secure_boot_check.cpp
+++ secure_boot_check.cpp
@@ -1,6 +1,12 @@
+#include "config.h"
+
 #include "utils.hpp"
 
 #include <phosphor-logging/lg2.hpp>
+
+#include <filesystem>
+#include <fstream>
+#include <string>
 
 PHOSPHOR_LOG2_USING;
 
@@ -20,6 +26,66 @@ int main()
     else
     {
         info("bmc-secure-boot found and indicates it is enabled");
+    }
+
+    // Now read the /sys/kernel/debug/aspeed/ files
+    std::string dbgVal;
+    std::ifstream dbgFile;
+    int secureBootVal = -1;
+    int abrImage = -1;
+
+    dbgFile.exceptions(std::ifstream::failbit | std::ifstream::badbit |
+                       std::ifstream::eofbit);
+
+    if (std::filesystem::exists(SYSFS_SECURE_BOOT_PATH))
+    {
+        try
+        {
+            dbgFile.open(SYSFS_SECURE_BOOT_PATH);
+            dbgFile >> dbgVal;
+            dbgFile.close();
+            info("Read {SECURE_BOOT_VAL} from secure_boot", "SECURE_BOOT_VAL",
+                 dbgVal);
+            secureBootVal = std::stoi(dbgVal);
+        }
+        catch (std::exception& e)
+        {
+            error("Failed to read secure boot sysfs file: {ERROR}", "ERROR", e);
+            // just continue and error will be logged at end if in mfg mode
+        }
+    }
+    else
+    {
+        info("sysfs file secure_boot not present");
+    }
+
+    if (std::filesystem::exists(SYSFS_ABR_IMAGE_PATH))
+    {
+
+        try
+        {
+            dbgFile.open(SYSFS_ABR_IMAGE_PATH);
+            dbgFile >> dbgVal;
+            dbgFile.close();
+            info("Read {ABR_IMAGE_VAL} from abr_image", "ABR_IMAGE_VAL",
+                 dbgVal);
+            abrImage = std::stoi(dbgVal);
+        }
+        catch (std::exception& e)
+        {
+            error("Failed to read abr image sysfs file: {ERROR}", "ERROR", e);
+            // just continue and error will be logged at end if in mfg mode
+        }
+    }
+    else
+    {
+        info("sysfs file abr_image not present");
+    }
+
+    if ((secureBootGpio != 1) || (secureBootVal != 1) || (abrImage != 0))
+    {
+        // TODO - Generate Error when in mfg mode
+        error("The system is not secure");
     }
 
     return 0;
