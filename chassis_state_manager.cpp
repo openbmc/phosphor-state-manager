09e88b481abc5518809f909bf3f379153fd01b5e
Andrew Geissler <geissonator@yahoo.com>
2022-03-14 15:46:14 -0500

    only run power restore on AC losses
    An internal defect was opened recently where it was noted that power
    restore functionality should only be run in situations where the chassis
    power was on prior to the BMC being reset and off when the BMC comes
    back up.
    
    This doesn't really align with the Redfish or PDI API we've defined
    though. Why would there be an option to "AlwaysOn" if the expected
    behavior was to not always execute the automatic power restore policy?
    
    Pushing this up for discussion but I doubt it will be something we merge
    as-is.
    
    Note that the one-time aspect of the power restore policy is always
    looked at. This is used for scenarios like inband code update where an
    automatic power on is always needed after the BMC reboot.
    
    Tested:
    - Verify no restore policy when not a power loss
    <6> One time not set, check user setting of power policy
    <6> Chassis power was not on prior to BMC reboot so do not run any power policy
    
    - Verify file is created on a blackout detection
    Mar 14 20:27:46 p10bmc phosphor-chassis-state-manager[3450]: The system suffered a power blackout.
    root@p10bmc:/lib/systemd/system# ls /run/openbmc/
    chassis@0-lost-power
    
    - Verify power restore is run when blackout occurs
    <6> One time not set, check user setting of power policy
    <6> Host power is off, processing power policy xyz.openbmc_project.Control.Power.RestorePolicy.Policy.AlwaysOff
    <6> power_policy=ALWAYS_POWER_OFF, set requested state to off
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>
    Change-Id: I882c056881bf6abde36bb11d3ae840c9b19d096a

--- chassis_state_manager.cpp
+++ chassis_state_manager.cpp
@@ -140,6 +140,15 @@ void Chassis::determineInitialState()
 
                     setStateChangeTime();
 
+                    // Generate file indicating AC loss occurred
+                    auto size =
+                        std::snprintf(nullptr, 0, CHASSIS_LOST_POWER_FILE, 0);
+                    size++; // null
+                    std::unique_ptr<char[]> buf(new char[size]);
+                    std::snprintf(buf.get(), size, CHASSIS_LOST_POWER_FILE, 0);
+                    std::ofstream outfile(buf.get());
+                    outfile.close();
+
                     if (phosphor::state::manager::utils::getGpioValue(
                             "reset-cause-pinhole") != 1)
                     {
