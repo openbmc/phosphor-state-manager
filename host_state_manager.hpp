3658c87c4a6dac98a7134307bf3810c4064fee33
NodeMan97 <corey.hardesty@icloud.com>
2022-05-09 23:56:39 -0500

    Host reboot configuration support
    Redfish API now supports a mechanism for the users of the system
    to configure the number of times a BMC should restart the host
    firmware when in an error path before giving up. Originally
    the function "attemptsLeft(uint32_t)" ignored the passed in value
    and and always set this number to a hardcoded 3(without the user
    knowing). Changes made here allow for this property to be set by
    an external user over D-bus for values other than 3.
    
    To compliment this functionality and the new DBus property
    "RetryAttempts" an override was created to ensure that when
    "RetryAttempts" is set by an external user that the "AttemptsLeft"
    property is updated accordingly.
    
    Testing:
      - built and ran with Docker along with the web changes.
      - verified update of "AttemptsLeft" through a p10bmc running
        on simics through busctl.
      - verified that when the "RetryAttempts" policy value is changed by
        either bmcweb or dbus set-property() that "AttemptsLeft" updates
        accordingly.
    
    busctl set-property xyz.openbmc_project.State.Host ... RetryAttempts u 1
    busctl get-property xyz.openbmc_project.State.Host ... RetryAttempts
    service@p10bmc: u 1
    
    busctl get-property xyz.openbmc_project.State.Host ... AttemptsLeft
    service@p10bmc: u 1
    
    
    curl -k -H "X-Auth-Token: $token" -X GET
    https://${BMC_IP}/redfish/v1/Systems/system/
    
    "Boot": {
        "AutomaticRetryAttempts": 1,
        "AutomaticRetryConfig": "RetryAttempts",
        "AutomaticRetryConfig@Redfish.AllowableValues": [
          "Disabled",
          "RetryAttempts"
        ],
        "RemainingAutomaticRetryAttempts": 1,
        "StopBootOnFault": "Never",
        "TrustedModuleRequiredToBoot": "Disabled"
      },
    
    curl -k -X PATCH  https://${BMC_IP}/redfish/v1/Systems/system/
    -d '{"RetryAttempts": 2}'
    
    "Boot": {
        "AutomaticRetryAttempts": 2,
        "AutomaticRetryConfig": "RetryAttempts",
        "AutomaticRetryConfig@Redfish.AllowableValues": [
          "Disabled",
          "RetryAttempts"
        ],
        "RemainingAutomaticRetryAttempts": 2,
        "StopBootOnFault": "Never",
        "TrustedModuleRequiredToBoot": "Disabled"
      },
    
    Signed-off-by: Corey Hardesty <corey.hardesty@icloud.com>
    Change-Id: I70a8457c88fb76e118937d79e8dbe15518df7264

--- host_state_manager.hpp
+++ host_state_manager.hpp
@@ -80,8 +80,6 @@ class Host : public HostInherit
         // Will throw exception on fail
         determineInitialState();
 
-        attemptsLeft(BOOT_COUNT_MAX_ALLOWED);
-
         // We deferred this until we could get our property correct
         this->emit_object_added();
     }
@@ -99,11 +97,38 @@ class Host : public HostInherit
     HostState currentHostState(HostState value) override;
 
     /**
+     * @brief Set value for allowable auto-reboot count
+     *
+     * This override is responsible for ensuring that when external users
+     * set the number of automatic retry attempts that the number of
+     * automatic reboot attempts left will update accordingly.
+     *
+     * @param[in] value - desired Reboot count value
+     *
+     * @return number of reboot attempts allowed.
+     */
+    uint32_t retryAttempts(uint32_t value) override
+    {
+        if (sdbusplus::xyz::openbmc_project::Control::Boot::server::
+                RebootAttempts::attemptsLeft() != value)
+        {
+            info("Automatic reboot retry attempts set to: {VALUE} ", "VALUE",
+                 value);
+            sdbusplus::xyz::openbmc_project::Control::Boot::server::
+                RebootAttempts::attemptsLeft(value);
+        }
+
+        return (sdbusplus::xyz::openbmc_project::Control::Boot::server::
+                    RebootAttempts::retryAttempts(value));
+    }
+
+    /**
      * @brief Set host reboot count to default
      *
      * OpenBMC software controls the number of allowed reboot attempts so
      * any external set request of this property will be overridden by
-     * this function and set to the default.
+     * this function and set to the number of the allowed auto-reboot
+     * retry attempts found on the system.
      *
      * The only code responsible for decrementing the boot count resides
      * within this process and that will use the sub class interface
@@ -111,15 +136,14 @@ class Host : public HostInherit
      *
      * @param[in] value      - Reboot count value, will be ignored
      *
-     * @return Default number of reboot attempts left
+     * @return number of reboot attempts left(allowed by retry attempts
+     * property)
      */
     uint32_t attemptsLeft(uint32_t value) override
     {
-        // value is ignored in this implementation
-        (void)(value);
         debug("External request to reset reboot count");
         return (sdbusplus::xyz::openbmc_project::Control::Boot::server::
-                    RebootAttempts::attemptsLeft(BOOT_COUNT_MAX_ALLOWED));
+                    RebootAttempts::attemptsLeft(value));
     }
 
   private:
@@ -227,7 +251,9 @@ class Host : public HostInherit
                                       server::Progress::bootProgress()),
                 convertForMessage(
                     sdbusplus::xyz::openbmc_project::State::OperatingSystem::
-                        server::Status::operatingSystemState()));
+                        server::Status::operatingSystemState()),
+                sdbusplus::xyz::openbmc_project::Control::Boot::server::
+                    RebootAttempts::retryAttempts());
     }
 
     /** @brief Function required by Cereal to perform deserialization.
@@ -245,7 +271,8 @@ class Host : public HostInherit
         std::string reqTranState;
         std::string bootProgress;
         std::string osState;
-        archive(reqTranState, bootProgress, osState);
+        uint32_t retryAttempts;
+        archive(reqTranState, bootProgress, osState, retryAttempts);
         auto reqTran = Host::convertTransitionFromString(reqTranState);
         // When restoring, set the requested state with persistent value
         // but don't call the override which would execute it
@@ -256,6 +283,8 @@ class Host : public HostInherit
         sdbusplus::xyz::openbmc_project::State::OperatingSystem::server::
             Status::operatingSystemState(
                 Host::convertOSStatusFromString(osState));
+        sdbusplus::xyz::openbmc_project::Control::Boot::server::RebootAttempts::
+            retryAttempts(retryAttempts);
     }
 
     /** @brief Serialize and persist requested host state
