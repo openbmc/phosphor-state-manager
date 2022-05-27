9991432446fb87c60a7a8950410618a526c44cb1
NodeMan97 <corey.hardesty@icloud.com>
2022-05-27 15:28:45 -0500

    Only run power restore on AC losses
    Added a meson build option that allows the customer to enable automatic
    power restore policy only for AC losses where the chassis power was
    on prior to a BMC reset(and off when it comes back online).
    
    The default is set to 'false' which provides the standard behavior of
    APR which is to always run APR on an BMC reboot.
    If the option 'only-run-apr-on-power-loss' is 'true' then APR is only
    run in the case of the chassis losing power. AC power losses trigger the
    creation of a file called 'chassis@0-lost-power' that identifies an
    AC power loss has occurred. This generated file can be found in the
    directory '/run/openbmc/'.
    
    Tested:
        - Built with option 'only-run-apr-on-power-loss' set to true.
        - When chassis power was off(without chassisStateChangeTime
          manipulation file did not appear). Same results as when compiled
          without 'only-run-apr-on-power-loss' enabled.
    
    root@p10bmc:/lib/systemd/system# systemctl start
                obmc-host-shutdown@0.target
    root@p10bmc:/lib/systemd/system# obmcutil state
    CurrentBMCState     : xyz.openbmc_project.State.BMC.BMCState.Ready
    CurrentPowerState   : xyz.openbmc_project.State.Chassis.PowerState.Off
    CurrentHostState    : xyz.openbmc_project.State.Host.HostState.Off
    
    root@p10bmc:/lib/systemd/system# systemctl
                stop xyz.openbmc_project.State.Chassis.service
    
    Jun 08 01:55:13 p10bmc phosphor-chassis-state-manager[3009]:
                The system suffered a power blackout.
    
    root@p10bmc:/lib/systemd/system# ls /run/openbmc/
    chassis@0-lost-power
    
    Signed-off-by: Corey Hardesty <corey.hardesty@icloud.com>
    Change-Id: Iac37761416d699e3a469936e00868a8d9b6dfdb1
    Signed-off-by: NodeMan97 <corey.hardesty@icloud.com>

--- chassis_state_manager.cpp
+++ chassis_state_manager.cpp
@@ -7,6 +7,7 @@
 #include "xyz/openbmc_project/State/Shutdown/Power/error.hpp"
 
 #include <fmt/format.h>
+#include <fmt/printf.h>
 
 #include <cereal/archives/json.hpp>
 #include <phosphor-logging/elog-errors.hpp>
@@ -42,14 +43,15 @@ constexpr auto CHASSIS_STATE_POWEROFF_TGT_FMT =
     "obmc-chassis-poweroff@{}.target";
 constexpr auto CHASSIS_STATE_HARD_POWEROFF_TGT_FMT =
     "obmc-chassis-hard-poweroff@{}.target";
-constexpr auto CHASSIS_STATE_POWERON_TGT_FMT = "obmc-chassis-poweron@{}.target";
-constexpr auto RESET_HOST_SENSORS_SVC_FMT =
-    "phosphor-reset-sensor-states@{}.service";
+constexpr auto CHASSIS_STATE_POWERON_TGT_FMT =
+    "obmc-chassis-poweron@{}.target";
+constexpr auto CHASSIS_BLACKOUT_TGT_FMT =
+    "obmc-chassis-blackout@{}.target";
 constexpr auto AUTO_POWER_RESTORE_SVC_FMT =
     "phosphor-discover-system-state@{}.service";
 constexpr auto ACTIVE_STATE = "active";
 constexpr auto ACTIVATING_STATE = "activating";
-
+  
 // Details at https://upower.freedesktop.org/docs/Device.html
 constexpr uint TYPE_UPS = 3;
 constexpr uint STATE_FULLY_CHARGED = 4;
@@ -152,11 +154,16 @@ void Chassis::determineInitialState()
                 {
                     info(
                         "Chassis power was on before the BMC reboot and it is off now");
-
-                    // Reset host sensors since system is off now
-                    startUnit(fmt::format(RESET_HOST_SENSORS_SVC_FMT, id));
+                    std::string chassis_lost_power_file_fmt =
+                         fmt::sprintf(CHASSIS_LOST_POWER_FILE, id);
+                    startUnit(fmt::format(CHASSIS_BLACKOUT_TGT_FMT, id));
 
                     setStateChangeTime();
+                    // Generate file indicating AC loss occurred
+                    fs::create_directories(BASE_FILE_DIR);
+                    fs::path chassisPowerLossFile{chassis_lost_power_file_fmt};
+                    std::ofstream outfile(chassisPowerLossFile);
+                    outfile.close();
 
                     // 0 indicates pinhole reset. 1 is NOT pinhole reset
                     if (phosphor::state::manager::utils::getGpioValue(
