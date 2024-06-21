836ccb3396d601fccebfe414e9f1d07a81d319db
Pavithra Barithaya <pavithrabarithaya07@gmail.com>
2024-06-21 09:07:11 -0500

    clang-tidy: Enable clang-analyzer-deadcode.DeadStores check
    This checks for values stored to variables that are never
    read afterwards.
    The change is made to avoid flagging this error when a unique_ptr
    is used rather make it a stack variable. The clang-tidy don't like
    the unused unique_ptr specifically rather than the unused class
    instance.
    
    Change-Id: I1c5a6b4032de75b7c8586467425f6fdacace5f31
    Signed-off-by: Pavithra Barithaya <pavithrabarithaya07@gmail.com>

--- host_condition_gpio/host_condition_main.cpp
+++ host_condition_gpio/host_condition_main.cpp
@@ -29,8 +29,7 @@ int main(int argc, char** argv)
     sdbusplus::server::manager_t objManager(bus, objGroupName.c_str());
 
     // For now, we only support checking Host0 status
-    auto host = std::make_unique<phosphor::condition::Host>(
-        bus, objPathInst.c_str(), hostId);
+    phosphor::condition::Host host(bus, objPathInst.c_str(), hostId);
 
     bus.request_name(busName.c_str());
 
