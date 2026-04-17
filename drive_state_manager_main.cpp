35e53630ae633b937dcbf986e456826ba709ce73
Eric Yang <eric.yang.wiwynn@gmail.com>
2026-04-17 11:12:37 +0000

    Add drive state manager daemon
    Add phosphor-drive-state-manager, a new per-instance daemon that
    implements the xyz.openbmc_project.State.Drive D-Bus interface for
    individual drive power control.
    
    The daemon maps RequestedDriveTransition to systemd targets and monitors
    JobRemoved signals to update CurrentDriveState, following the same
    pattern as the existing chassis and host state managers. Supported
    transitions are On, Off, Reboot, HardReboot, and Powercycle.
    
    Drive instances use string identifiers (e.g. nvme0) to align with
    Redfish DriveId. Seven systemd target templates are included for
    transition, state, and marker operations. Platform-specific hardware
    operations attach to these targets via .wants/ directories, keeping all
    hardware logic out of the state manager.
    
    Motivation:
    On storage-dense platforms, there is a practical need to power cycle or
    reset individual NVMe/SSD drives from the BMC independently of chassis
    or host state. The State.Drive interface already exists in
    phosphor-dbus-interfaces [1] but had no upstream consumer. This daemon
    provides that consumer using the established PSM systemd target
    orchestration pattern.
    
    Reference:
    [1] https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/43155
    
    Depends-On: I13ab50a771f4bcf678308b53e48c6084925ca8a1
    Change-Id: Iccc69acf71ce135481c8a59124072ee460be12a5
    Signed-off-by: Eric Yang <eric.yang.wiwynn@gmail.com>

---
+++ drive_state_manager_main.cpp
@@ -0,0 +1,64 @@
+#include "drive_state_manager.hpp"
+
+#include <getopt.h>
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/bus.hpp>
+
+#include <format>
+#include <string>
+
+PHOSPHOR_LOG2_USING;
+
+using DriveState = sdbusplus::server::xyz::openbmc_project::state::Drive;
+
+int main(int argc, char** argv)
+{
+    std::string driveInstance;
+    int arg;
+    int optIndex = 0;
+
+    static struct option longOpts[] = {
+        {"drive", required_argument, nullptr, 'd'}, {nullptr, 0, nullptr, 0}};
+
+    while ((arg = getopt_long(argc, argv, "d:", longOpts, &optIndex)) != -1)
+    {
+        switch (arg)
+        {
+            case 'd':
+                driveInstance = optarg;
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (driveInstance.empty())
+    {
+        error("Drive instance name is required (--drive <name>)");
+        return 1;
+    }
+
+    auto bus = sdbusplus::bus::new_default();
+
+    const auto* objPath = DriveState::namespace_path::value;
+    auto driveBasePath = sdbusplus::message::object_path(objPath) /
+                         std::string(DriveState::namespace_path::drive);
+    auto driveBusName =
+        std::string(DriveState::interface) + "." + driveInstance;
+    std::string objPathInst = driveBasePath / driveInstance;
+
+    sdbusplus::server::manager_t objManager(bus, driveBasePath.str.c_str());
+    phosphor::state::manager::Drive manager(bus, objPathInst.c_str(),
+                                            driveInstance);
+
+    bus.request_name(driveBusName.c_str());
+
+    while (true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+
+    return 0;
+}
