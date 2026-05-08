openbmc/phosphor-state-manager

commit 341d540b860f3de2eea64e7e4ecf4a57503e42c7

refactor only-run-apr-on-power-loss
Remove preprocessor usage for this meson option.

Conditional compilation leads to some code not being parsed and
typechecked, and results in lower coverage in static analysis.

Migrate the option to use if-constexpr instead.

Tested: Inspection only.

Change-Id: I4a89b2ec6ea3d4c81323b6935f37fa5e8016dd6b
Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

discover_system_state.cpp
@@ -227,14 +227,15 @@ int main(int argc, char** argv)
         // Always execute power on if AlwaysOn is set, otherwise check config
         // option (and AC loss status) on whether to execute other policy
         // settings
-#if ONLY_RUN_APR_ON_POWER_LOSS
-        else if (!phosphor::state::manager::utils::checkACLoss(hostId))
+        else if constexpr (ONLY_RUN_APR_ON_POWER_LOSS)
         {
-            info(
-                "Chassis power was not on prior to BMC reboot so do not run any further power policy");
-            return 0;
+            if (!phosphor::state::manager::utils::checkACLoss(hostId))
+            {
+                info(
+                    "Chassis power was not on prior to BMC reboot so do not run any further power policy");
+                return 0;
+            }
         }
-#endif
         else if (RestorePolicy::Policy::AlwaysOff ==
                  RestorePolicy::convertPolicyFromString(powerPolicy))
         {

commit 28ed73cc2d3088f8d1a85bf9615fe2f3264b8277

discover-system-state: Fix non-AlwaysOn policy flow
When ONLY_RUN_APR_ON_POWER_LOSS is enabled, the AC loss check was
handled in an else-if branch before the non-AlwaysOn policy logic.
This caused policies such as AlwaysOff and Restore to be skipped.

Run the AC loss guard first, then continue evaluating the configured
power policy when AC loss is detected.

Change-Id: Ieafb11aec4c73f49bfbc01ed53efe930c9560459
Fixes: 341d540b860f ("refactor only-run-apr-on-power-loss")
Signed-off-by: Angela Cheng wiwynn <angelaptcheng@gmail.com>

discover_system_state.cpp
@@ -231,59 +231,66 @@ int main(int argc, char** argv)
         // Always execute power on if AlwaysOn is set, otherwise check config
         // option (and AC loss status) on whether to execute other policy
         // settings
-        else if constexpr (ONLY_RUN_APR_ON_POWER_LOSS)
+        else
         {
-            if (!phosphor::state::manager::utils::checkACLoss(hostId))
+            if constexpr (ONLY_RUN_APR_ON_POWER_LOSS)
+            {
+                if (!phosphor::state::manager::utils::checkACLoss(hostId))
+                {
+                    info(
+                        "Chassis power was not on prior to BMC reboot so do not run any further power policy");
+                    return 0;
+                }
+            }
+            if (RestorePolicy::Policy::AlwaysOff ==
+                RestorePolicy::convertPolicyFromString(powerPolicy))
             {
                 info(
-                    "Chassis power was not on prior to BMC reboot so do not run any further power policy");
-                return 0;
+                    "power_policy=ALWAYS_POWER_OFF, set requested state to off ({DELAY}s delay)",
+                    "DELAY", powerRestoreDelaySec.count());
+                applyPowerRestoreDelay(bus, powerRestoreDelayUsec);
+                // Read last requested state and re-request it to execute it
+                auto hostReqState =
+                    phosphor::state::manager::utils::getProperty(
+                        bus, hostPath, HostState::interface,
+                        HostState::property_names::requested_host_transition);
+                if (hostReqState !=
+                    convertForMessage(server::Host::Transition::Off))
+                {
+                    phosphor::state::manager::utils::setProperty(
+                        bus, hostPath, HostState::interface,
+                        HostState::property_names::requested_host_transition,
+                        convertForMessage(server::Host::Transition::Off));
+                }
             }
-        }
-        else if (RestorePolicy::Policy::AlwaysOff ==
-                 RestorePolicy::convertPolicyFromString(powerPolicy))
-        {
-            info(
-                "power_policy=ALWAYS_POWER_OFF, set requested state to off ({DELAY}s delay)",
-                "DELAY", powerRestoreDelaySec.count());
-            applyPowerRestoreDelay(bus, powerRestoreDelayUsec);
-            // Read last requested state and re-request it to execute it
-            auto hostReqState = phosphor::state::manager::utils::getProperty(
-                bus, hostPath, HostState::interface,
-                HostState::property_names::requested_host_transition);
-            if (hostReqState !=
-                convertForMessage(server::Host::Transition::Off))
+            else if (RestorePolicy::Policy::Restore ==
+                     RestorePolicy::convertPolicyFromString(powerPolicy))
             {
-                phosphor::state::manager::utils::setProperty(
-                    bus, hostPath, HostState::interface,
-                    HostState::property_names::requested_host_transition,
-                    convertForMessage(server::Host::Transition::Off));
-            }
-        }
-        else if (RestorePolicy::Policy::Restore ==
-                 RestorePolicy::convertPolicyFromString(powerPolicy))
-        {
-            info("power_policy=RESTORE, restoring last state ({DELAY}s delay)",
-                 "DELAY", powerRestoreDelaySec.count());
-            applyPowerRestoreDelay(bus, powerRestoreDelayUsec);
-            // Read last requested state and re-request it to execute it
-            auto hostReqState = phosphor::state::manager::utils::getProperty(
-                bus, hostPath, HostState::interface,
-                HostState::property_names::requested_host_transition);
+                info(
+                    "power_policy=RESTORE, restoring last state ({DELAY}s delay)",
+                    "DELAY", powerRestoreDelaySec.count());
+                applyPowerRestoreDelay(bus, powerRestoreDelayUsec);
+                // Read last requested state and re-request it to execute it
+                auto hostReqState =
+                    phosphor::state::manager::utils::getProperty(
+                        bus, hostPath, HostState::interface,
+                        HostState::property_names::requested_host_transition);
 
-            // As long as the host transition is not 'Off' power on host state.
-            if (hostReqState !=
-                convertForMessage(server::Host::Transition::Off))
-            {
-                phosphor::state::manager::utils::setProperty(
-                    bus, hostPath, HostState::interface,
-                    HostState::property_names::restart_cause,
-                    convertForMessage(
-                        server::Host::RestartCause::PowerPolicyPreviousState));
-                phosphor::state::manager::utils::setProperty(
-                    bus, hostPath, HostState::interface,
-                    HostState::property_names::requested_host_transition,
-                    convertForMessage(server::Host::Transition::On));
+                // As long as the host transition is not 'Off' power on host
+                // state.
+                if (hostReqState !=
+                    convertForMessage(server::Host::Transition::Off))
+                {
+                    phosphor::state::manager::utils::setProperty(
+                        bus, hostPath, HostState::interface,
+                        HostState::property_names::restart_cause,
+                        convertForMessage(server::Host::RestartCause::
+                                              PowerPolicyPreviousState));
+                    phosphor::state::manager::utils::setProperty(
+                        bus, hostPath, HostState::interface,
+                        HostState::property_names::requested_host_transition,
+                        convertForMessage(server::Host::Transition::On));
+                }
             }
         }
     }