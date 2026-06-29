b2ff78b798caa41247b42b23bf0fda1b58c0718e
Ramya Sivakumar <sramya@ami.com>
2026-06-29 05:49:51 +0000

    Add error handling to bus.call_noreply() calls
    Added all bus.call_noreply() calls with try-catch blocks with throw and
    error logging in state manager and utils files
    
    Change-Id: Ic614e6f213aa825afb7c06b7f97c9418aa907931
    Signed-off-by: Ramya Sivakumar <sramya@ami.com>

--- chassis_state_manager.cpp
+++ chassis_state_manager.cpp
@@ -490,8 +490,16 @@ void Chassis::startUnit(const std::string& sysdUnit)
     method.append(sysdUnit);
     method.append("replace");
 
-    this->bus.call_noreply(method);
-
+    try
+    {
+        this->bus.call_noreply(method);
+    }
+    catch (const sdbusplus::exception_t& e)
+    {
+        error("Failed to start unit {UNIT}, exception:{ERROR}", "UNIT",
+              sysdUnit, "ERROR", e);
+        throw;
+    }
     return;
 }
 
@@ -504,8 +512,16 @@ void Chassis::restartUnit(const std::string& sysdUnit)
     method.append(sysdUnit);
     method.append("replace");
 
-    this->bus.call_noreply(method);
-
+    try
+    {
+        this->bus.call_noreply(method);
+    }
+    catch (const sdbusplus::exception_t& e)
+    {
+        error("Failed to restart unit {UNIT}, exception:{ERROR}", "UNIT",
+              sysdUnit, "ERROR", e);
+        throw;
+    }
     return;
 }
 
