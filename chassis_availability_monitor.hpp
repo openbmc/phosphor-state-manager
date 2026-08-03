8669794adc38b64a3e7dcce16435eb6adc74a441
Alex Countryman <alexjc@fastmail.com>
2026-08-03 05:44:50 +0000

    chassis-availability: handle chassis removal events
    Add subscribeToChassisRemoved() and onChassisRemoved() methods.
    Together, these methods monitor for the removal of a chassis from
    a system, and unsubscribe to the appropriate services.
    
    Tested: Ran unit tests on phosphor-state-manager repo. All tests passed.
    
    Change-Id: I582f07cfe8f93ca7b8063541277189f023ac1433
    Signed-off-by: Alex Countryman <alexjc@fastmail.com>

--- chassis_availability_monitor.hpp
+++ chassis_availability_monitor.hpp
@@ -66,6 +66,9 @@ class ChassisAvailability
     /** @brief Subscribe to chassis hot-plug events */
     void subscribeToChassisAdded();
 
+    /** @brief Subscribe to chassis removal events */
+    void subscribeToChassisRemoved();
+
     /** @brief Set up monitoring for a specific chassis
      * @param[in] chassisNum Chassis number to set up monitoring for
      */
@@ -89,6 +92,11 @@ class ChassisAvailability
      * @param[in] msg D-Bus InterfacesAdded message
      */
     void onChassisAdded(sdbusplus::message_t& msg);
+
+    /** @brief Handle chassis removal
+     * @param[in] msg D-Bus InterfacesRemoved message
+     */
+    void onChassisRemoved(sdbusplus::message_t& msg);
 
     /** @brief Extract chassis number from D-bus object path
      * @param[in] path D-Bus object path to extract chassis number from
@@ -127,11 +135,11 @@ class ChassisAvailability
      */
     std::map<int, ChassisState> chassisStates;
 
-    /** @brief D-Bus signal matches for PropertiesChanged subscriptions */
-    std::vector<std::unique_ptr<sdbusplus::bus::match_t>> propertyMatches;
-
     /** @brief D-bus signal match for a new added chassis */
     std::unique_ptr<sdbusplus::bus::match_t> chassisAddedMatch;
+
+    /** @brief D-bus signal match for chassis removal */
+    std::unique_ptr<sdbusplus::bus::match_t> chassisRemovedMatch;
 };
 
 } // namespace manager
